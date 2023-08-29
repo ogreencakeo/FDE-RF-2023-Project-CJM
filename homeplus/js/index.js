eb34a1962a5c8aa825eafd1f7a7915077e82595e
jimin0427 <cjm0427@hs.ac.kr>
2023-08-29 21:49:09 +0900

    .

--- homeplus/js/index.js
+++ homeplus/js/index.js
@@ -1,30 +1,36 @@
 
-window.addEventListener('load', exhibitionFn);
+const exhibition_arr = [
+    즉시배송,
+    등급혜택,
+    이벤트/쿠폰,
+    모바일교환권,
+    하나더,
+    물가안정365,
+    홈플식탁,
+    홈플러스시그니처,
+    산지직
+];
 
-function exhibitionFn(){
-
-    const move_to_exhibition1 = document.querySelector('.move-to-exhibition');
-    const exhibition_arr = [
-        즉시배송,
-        등급혜택,
-        이벤트/쿠폰,
-        모바일교환권,
-        하나더,
-        물가안정365,
-        홈플식탁,
-        홈플러스시그니처,
-        산지직
-    ];
-    
+window.addEventListener('load', function(){
+    const move_to_exhibition = document.querySelector('.move-to-exhibition');
     let hcode = '';
 
+    hcode += '<nav><ul>';
     for(let i=0; i<exhibition_arr.length; i++){
-        console.log(move_to_exhibition[i]);
-        console.log(exhibition_arr[i]);
-        move_to_exhibition2[i].innerHTML = `
-                <a href="#"><img src="./images/move-to-exhibition${i+1}.png" alt="기획전 이미지"> ${exhibition_arr[i]}</a>
+        hcode += `
+            <li>
+                <a href = "#"></a>
+            </li>
+        `;
+    }
+    hcode += '</ul></nav>';
+    move_to_exhibition.innerHTML = hcode;
+
+    const exhibition_li = move_to_exhibition.querySelectorAll('li a');
+    for(let i=0; i<exhibition_li.length; i++){
+        exhibition_li[i].innerHTML += `
+            <img src="./images/move-to-exhibition${i+1}.png" alt="">${exhibition_arr[i]}
         `;
     }
 
-    
-}
+});
