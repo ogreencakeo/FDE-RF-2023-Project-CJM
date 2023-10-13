33a15f7996dca94d7128b8f1265664c8f7330573
jimin0427 <cjm0427@hs.ac.kr>
2023-10-13 19:59:27 +0900

    asd

--- universal/js/index.js
+++ universal/js/index.js
@@ -5,3 +5,10 @@ setTimeout(()=>{
     main_img_stage.classList.add('on');
 },400);
 
+// universal 글자 물결
+const universal_span_mave = 'Universal Studios';
+const wave_span_code = dFn.qs('.universal-span-move');
+
+universal_span_mave.forEach((ele)=>{
+    wave_span_code.innerHTML += `${this}`;
+})
