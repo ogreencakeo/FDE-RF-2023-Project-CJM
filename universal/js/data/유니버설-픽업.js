c552219cfd8b4b2aa7d9fcf36484c631a0c4829f
ogreencakeo <magente2000@gmail.com>
2023-10-20 18:05:32 +0900

    ads

--- universal/js/data/유니버설-픽업.js
+++ universal/js/data/유니버설-픽업.js
@@ -3,62 +3,109 @@ const pickUp1 = {
     "title" : "할로윈 호러 나이트",
     "img" : "main/pickup/picup1.jpg",
     "holdingTime" : "개최기간：2023년9월 8일(금) ~ 11월 5일(일)",
-    "content" : `역대 최다 좀비&최고로 흉악한 호러의 총집결!<br/>뱃속 깊은 곳에서 터져 나오는 절규로 뼛속까지 상쾌하게!`
+    "content" : `역대 최다 좀비&최고로 흉악한 호러의 총집결!<br/>뱃속 깊은 곳에서 터져 나오는 절규로 뼛속까지 상쾌하게!`,
+    "contentImg" : "main/pickup/pickImg1.jpg"
 }
 const pickUp2 = {
     "title" : "하하하! 할로윈 파티",
     "img" : "main/pickup/picup2.jpg",
     "holdingTime" : "개최기간：2023년9월 7일(목) ~ 11월 5일(일)",
-    "content" : "귀엽고 즐거워서 웃음꽃이 만발하는 최고로 기분 좋은 열광 데이 타임!"
+    "content" : "귀엽고 즐거워서 웃음꽃이 만발하는 최고로 기분 좋은 열광 데이 타임!",
+    "contentImg" : "main/pickup/pickImg2.jpg"
 }
 const pickUp3 = {
-    title : "“슈퍼 닌텐도 월드™”",
-    img : "main/pickup/picup3.jpg",
-    holdingTime : "현재 운영 중!",
-    content :
+    "title" : "“슈퍼 닌텐도 월드™”",
+    "img" : "main/pickup/picup3.jpg",
+    "holdingTime" : "현재 운영 중!",
+    "content" : "이곳은 어린이는 물론 어른까지 모두가 오감을 전부 개방해서 마음껏 놀 수 있는 전 세계에서 유일한 놀이터. 암호는 ‘#WE ARE MARIO!!’이제 놀이의 본능을 발산해 보세요!",
+    "contentImg" : ""
 }
-pickUp4 : {
-    title : "“NO LIMIT! 퍼레이드”",
-    img : "main/pickup/picup4.jpg",
-    holdingTime : "개최기간：2023년3월 1일(수) ~",
-    content :
+const pickUp4 = {
+    "title" : "“NO LIMIT! 퍼레이드”",
+    "img" : "main/pickup/picup4.jpg",
+    "holdingTime" : "개최기간：2023년3월 1일(수) ~",
+    "content" : "최고로 핫한 댄스파티가 시작된다! 처음으로 등장하는 마리오와 포켓몬을 비롯해 미니언과 'SING'의 멤버들, 엘모, 스누피, 헬로키티가 왁자지껄 퍼레이드카&어깨가 들썩이는 음악과 함께 속속 등장",
+    "contentImg" : "main/pickup/pickImg4.jpg"
+
 }
-pickUp5 : {
-    title : "매지컬 크리처스 인카운터 ~마법 생물과의 만남~",
-    img : "main/pickup/picup5.jpg",
-    holdingTime : "개최기간：2023년3월 17일(금) ~ 12월 31일(일)",
-    content :
+const pickUp5 = {
+    "title" : "매지컬 크리처스 인카운터 ~마법 생물과의 만남~",
+    "img" : "main/pickup/picup5.jpg",
+    "holdingTime" : "개최기간：2023년3월 17일(금) ~ 12월 31일(일)",
+    "content" : "드디어 마법 생물도 살아 숨쉬기 시작했어요! 위저딩 월드 오브 해리 포터™, 끊임없이 변화하는 이 세계에서, 당신의 마법 여행은 계속됩니다.",
+    "contentImg" : "main/pickup/pickImg5.png"
 }
-pickUp6 : {
-    title : "“주술회전 더 리얼 4-D”",
-    img : "main/pickup/picup6.jpg",
-    holdingTime : "개최기간：2022년9월 16일(금) ~ 2023년11월 5일(일)",
-    content :
+const pickUp6 = {
+    "title" : "“주술회전 더 리얼 4-D”",
+    "img" : "main/pickup/picup6.jpg",
+    "holdingTime" : "개최기간：2022년9월 16일(금) ~ 2023년11월 5일(일)",
+    "content" : "바로 눈앞에서 주술전과 조우하는 충격의 4-D 어트랙션, 등장. 완전 오리지널 3D 영상×진동과 물보라 등의 특수효과를 통해, 섬뜩한 형상을 하고 덮쳐 오는 주령과 주술사들",
+    "contentImg" : "main/pickup/pickImg6.png"
 }
-pickUp7 : {
-    title : "NO LIMIT! 크리스마스",
-    img : "main/pickup/picup7.jpg",
-    holdingTime : "개최기간：2023년11월 21일(화) ~ 2024년1월 8일(월)",
-    content : "USJ의 크리스마스는 모든 것이 새롭고 자극적! 지금만 즐길 수 있는 겨울 장식과 흥분되는 마리오의 세계에서 전력을 다해 놀며 “YEAH!” 눈으로 곱게 화장한 마법 세계에서 마법 생물들과 소통하며 “YEAH!” 밤에는 새롭게 장식한 특대형 트리 아래, 엄청나게 화려한 크리스마스 파티에서 빛나는 굿즈를 손에 들고 자신이 직접 반짝반짝 빛나는 빛이 되어 “OH! YEAH!” 넋 놓고 바라만 보는 건 재미없지! 이곳만의 최고로 핫한 크리스마스에 온몸으로 뛰어들자!",
+const pickUp7 = {
+    "title" : "NO LIMIT! 크리스마스",
+    "img" : "main/pickup/picup7.jpg",
+    "holdingTime" : "개최기간：2023년11월 21일(화) ~ 2024년1월 8일(월)",
+    "content" : "USJ의 크리스마스는 모든 것이 새롭고 자극적! 지금만 즐길 수 있는 겨울 장식과 흥분되는 마리오의 세계에서 전력을 다해 놀며 “YEAH!” 눈으로 곱게 화장한 마법 세계에서 마법 생물들과 소통하며 “YEAH!” 밤에는 새롭게 장식한 특대형 트리 아래, 엄청나게 화려한 크리스마스 파티에서 빛나는 굿즈를 손에 들고 자신이 직접 반짝반짝 빛나는 빛이 되어 “OH! YEAH!” 넋 놓고 바라만 보는 건 재미없지! 이곳만의 최고로 핫한 크리스마스에 온몸으로 뛰어들자!",
+    "contentImg" : "main/pickup/pickImg7.jpg"
 }
-pickUp8 : {
-    title : "NO LIMIT! 카운트다운 2024",
-    img : "main/pickup/picup8.jpg",
-    holdingTime : "개최기간：2023년12월 31일 (일) 19:00 ~ 2024년1월 1일 (월) 21:00",
-    content :
+const pickUp8 = {
+    "title" : "NO LIMIT! 카운트다운 2024",
+    "img" : "main/pickup/picup8.jpg",
+    "holdingTime" : "개최기간：2023년12월 31일 (일) 19:00 ~ 2024년1월 1일 (월) 21:00",
+    "content" : "파크의 친구들과 미니언, 그리고 “원피스”부터 좀비 군단까지, 엔터테인먼트가 총집결한 하룻밤 한정의 스페셜 스테이지는 물론, 모두가 하나 되어 열광할 수 있는 카운트다운 모멘트, 게다가 인기 어트랙션의 26시간 무제한 탑승까지!",
+    "contentImg" : "main/pickup/pickImg8.jpg"
 }
 
-pickUp9 : {
-    title : "‘스파이더맨 더 라이드’ 파이널 이벤트",
-    img : "main/pickup/picup9.jpg",
-    holdingTime : "개최기간：2023년7월 4일(화) ~ 2024년1월 22일(월)",
-    content :
+const pickUp9 = {
+    "title" : "‘스파이더맨 더 라이드’ 파이널 이벤트",
+    "img" : "main/pickup/picup9.jpg",
+    "holdingTime" : "개최기간：2023년7월 4일(화) ~ 2024년1월 22일(월)",
+    "content" : "상상을 초월하는 스릴감과 차원이 다른 몰입도로 7년 연속 ‘세계 NO.1 라이드’라는 칭호를 받은 어메이징 어드벤처 오브 스파이더맨 더 라이드가 드디어 파이널을 맞이합니다! 건물에서 건물로 공중을 질주하는 극한의 스릴과 눈앞에 닥친 숨 막히는 배틀도, 이제 라스트를 맞이합니다.",
+    "contentImg" : "main/pickup/pickImg8.jpg"
 }
-pickUp10 : {
-    title : "“NO LIMIT!”",
-    img : "main/pickup/picup10.jpg",
-    holdingTime : "개최기간：2022년3월 29일(화) ~ <br/> (일본어 페이지 한정)",
-    content :
+const pickUp10 = {
+    "title" : "“NO LIMIT!”",
+    "img" : "main/pickup/picup10.jpg",
+    "holdingTime" : "개최기간：2022년3월 29일(화) ~ <br/> (일본어 페이지 한정)",
+    "content" : "여기에서 모든 것을 잊어도 좋다. 여기에서는 자신의 껍질을 깰 수 있습니다. 상상을 초월한 엔터테인먼트가",
+    "contentImg" : "main/pickup/pickImg10.jpg"
+}
+
+const parkjoin1 = {
+    "title" : "파크를 즐기는 방법",
+    "img" : "main/pickup/join-main.jpg",
+    "subTitle" : "생일·기념일을 파크에서 축하!",
+    "content" : `버스데이 스티커로 오늘의 주인공은 당신!<br/>숍 직원과 크루에게 버스데이 스티커를 받으면 ‘생일 축하해요!’라는 축하의 말을 듣거나 캐릭터가 다가올지도!?`,
+    "caution" : `※ 배포 장소는 게스트 서비스 또는 크루에게 문의해 주십시오.<br/>※ 스티커 이미지는 예고 없이 변경될 수 있습니다. (2020년 3월 시점)`,
+}
+const parkjoin2 = {
+    "title" : "",
+    "img" : "",
+    "subTitle" : "",
+    "content" : "",
+    "caution" : "",
+}
+const parkjoin3 = {
+    "title" : "",
+    "img" : "",
+    "subTitle" : "",
+    "content" : "",
+    "caution" : "",
+}
+const parkjoin4 = {
+    "title" : "",
+    "img" : "",
+    "subTitle" : "",
+    "content" : "",
+    "caution" : "",
+}
+const parkjoin5 = {
+    "title" : "",
+    "img" : "",
+    "subTitle" : "",
+    "content" : "",
+    "caution" : "",
 }
 
 // (유익한 정보)
