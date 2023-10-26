2ce42fd71a3a50994f96144931cc45573df19728
ogreencakeo <magente2000@gmail.com>
2023-10-26 17:34:45 +0900

    sad

---
+++ universal/js/data/유니버설-시즈널메뉴.js
@@ -0,0 +1,26 @@
+
+/*
+    location : 판매장소
+    service_type : 서비스 타입
+    payment_method : 결제 방법
+    menu_provided : 제공 메뉴
+    japanese_page : 일본어 페이지
+    englisgh_page : 영어 페이지
+    seasonal_img : 메뉴 이미지
+    seasonal_main_img : 메인 이미지
+    seasonal_logo : 로고 이미지
+*/
+const seasonalMenu = {
+    "seasonalMenu1" : {
+        "location" : "스튜디오 스타즈 레스토랑",
+        "service_type" : "퀵서비스",
+        "payment_method" : "신용카드 사용 가능",
+        "menu_provided" : `저 알레르기 메뉴(일본어 페이지 한정),<br/> 어린이 메뉴, 양식, 식사류,이유식 있음,<br/> 돼지고기 미사용·무알코올 메뉴(영어 페이지 한정)`,
+        "japanese_page" : ``,
+        "englisgh_page" : ``,
+        "seasonal_img" : [""],
+        "seasonal_logo" : ,
+        "seasonal_main_img" : 
+    },
+    
+}
