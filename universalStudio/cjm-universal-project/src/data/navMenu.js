ae1370ab48b00ea92d3e0db30bba2003446cb8f6
ogreencakeo <magente2000@gmail.com>
2023-11-14 17:57:10 +0900

    Update navMenu.js

--- universalStudio/cjm-universal-project/src/data/navMenu.js
+++ universalStudio/cjm-universal-project/src/data/navMenu.js
@@ -5,7 +5,41 @@ export const navMenu = [
     },
     {
         txt : '시즈널메뉴',
-        link : '/seasonalMenu'
+        link : '/seasonalMenu',
+        sub : [
+            {
+                txt : '스튜디오 스타즈 레스토랑',
+                link : '/seamenu1'
+            },
+            {
+                txt : '피네간즈 바&그릴',
+                link : '/seamenu2'
+            },
+            {
+                txt : '루이즈 N.Y. 피자 팔러',
+                link : '/seamenu3'
+            },
+            {
+                txt : '멜즈 드라이브인',
+                link : '/seamenu4'
+            },
+            {
+                txt : '스리 브룸스틱스™',
+                link : '/seamenu5'
+            },
+            {
+                txt : '스누피™ 백롯 카페',
+                link : '/seamenu6'
+            },
+            {
+                txt : '해피니스 카페',
+                link : '/seamenu7'
+            },
+            {
+                txt : '파크 사이드 그릴',
+                link : '/seamenu8'
+            },
+        ]
     },
     {
         txt : '굿즈',
