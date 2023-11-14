0a8d3f0d490f14705143c9be9ad926bf02be1bd3
ogreencakeo <magente2000@gmail.com>
2023-11-14 17:47:03 +0900

    sdf

--- universalStudio/cjm-universal-project/src/index.js
+++ universalStudio/cjm-universal-project/src/index.js
@@ -3,6 +3,9 @@ import ReactDOM from 'react-dom/client';
 import {BrowserRouter, Route, Routes} from "react-router-dom";
 
 import {Main} from './contents/Main.jsx'
+import {Event} from './contents/Event.jsx'
+import {SeasonalMenu} from './contents/SeasonalMenu.jsx'
+import {CustomerCenter} from './contents/CustomerCenter.jsx'
 
 // CSS 불러오기
 import './css/common.css';
@@ -15,6 +18,10 @@ export default function App() {
       <Routes>
         <Route path='/' element={<Layout/>}>
           <Route index element={<Main />} />
+          <Route path='/event' element={<Event />} />
+          <Route path='/seasonalMenu' element={<SeasonalMenu />} />
+          <Route path='/goods' element={<Goods />} />
+          <Route path='/customerCenter' element={<CustomerCenter />} />
         </Route>
       </Routes>
     </BrowserRouter>
