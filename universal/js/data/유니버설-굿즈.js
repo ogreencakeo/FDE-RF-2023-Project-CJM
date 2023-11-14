a57c5ab32c43bd1530fd035894617059f5b80588
ogreencakeo <magente2000@gmail.com>
2023-11-14 17:28:13 +0900

    Update 유니버설-굿즈.js

--- universal/js/data/유니버설-굿즈.js
+++ universal/js/data/유니버설-굿즈.js
@@ -1,5 +1,492 @@
 export const goods =[
     {
-        "" : 
-    }
+        supermario : [
+            {
+                "store" : "슈퍼 닌텐도 월드™ 입구 부근 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "파워 업 밴드",
+                "content" :"※ 파워 업 밴드는 슈퍼 닌텐도 월드™ 입구 부근의 카트 등에서 미리 구매하시는 것을 추천합니다.",
+                "image" : "goods1/goods1.jpg"
+            },
+            {
+                "store" : "슈퍼 닌텐도 월드™ 입구 부근 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "파워 업 밴드",
+                "content" :"※ 파워 업 밴드는 슈퍼 닌텐도 월드™ 입구 부근의 카트 등에서 미리 구매하시는 것을 추천합니다.",
+                "image" : "goods1/goods2.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "쿠션",
+                "content" :"",
+                "image" : "goods1/goods3.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "봉제인형 모자",
+                "content" :"",
+                "image" : "goods1/goods4.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "쿠키 캔 세트(4캔)",
+                "content" :"",
+                "image" : "goods1/goods5.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "볼펜",
+                "content" :"",
+                "image" : "goods1/goods6.jpg"
+            },
+            {
+                "store" : "마리오 모터스™",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "머리띠",
+                "content" :"",
+                "image" : "goods1/goods7.jpg"
+            },
+            {
+                "store" : "마리오 모터스™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "종합 디저트",
+                "content" :"",
+                "image" : "goods1/goods8.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "봉제인형",
+                "content" :"",
+                "image" : "goods1/goods9.jpg"
+            },
+            {
+                "store" : "유니버설 스튜디오 스토어",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "월간 메달리온(11월)",
+                "content" :"※ 수베니어 메달리온은 모두 구리색입니다.",
+                "image" : "goods1/goods10.jpg"
+            },
+            {
+                "store" : "마리오 모터스™",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "펜 케이스",
+                "content" :"",
+                "image" : "goods1/goods11.jpg"
+            },
+            {
+                "store" : "1UP 팩토리™ 등",
+                "itemGoods" : "슈퍼 닌텐도 월드™",
+                "usage" : "쿠션",
+                "content" :"",
+                "image" : "goods1/goods12.jpg"
+            },
+        ]
+    },
+    {
+        snoopy : [
+            {
+                "store" : "유니버설 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "에코백",
+                "content" :"",
+                "image" : "goods2/goods1.jpg"
+            },
+            {
+                "store" : "유니버설 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "마스코트 키 체인",
+                "content" :"",
+                "image" : "goods2/goods2.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "프린트 쿠키",
+                "content" :"",
+                "image" : "goods2/goods3.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "센베이 버스",
+                "content" :"",
+                "image" : "goods2/goods4.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어",
+                "itemGoods" : "스누피",
+                "usage" : "메달리온(스누피·총 3종)",
+                "content" :"※ 수베니어 메달리온은 모두 구리색입니다.",
+                "image" : "goods2/goods5.jpg"
+            },
+            {
+                "store" : "캘리포니아 과자점",
+                "itemGoods" : "스누피",
+                "usage" : "메달리온(스누피 총 3종)",
+                "content" :"※ 수베니어 메달리온은 모두 구리색입니다.",
+                "image" : "goods2/goods6.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "헤어밴드",
+                "content" :"",
+                "image" : "goods2/goods7.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "버킷햇",
+                "content" :"",
+                "image" : "goods2/goods8.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "담요",
+                "content" :"",
+                "image" : "goods2/goods9.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "담요",
+                "content" :"",
+                "image" : "goods2/goods10.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "베레모",
+                "content" :"",
+                "image" : "goods2/goods11.jpg"
+            },
+            {
+                "store" : "스누피 스튜디오 스토어 등",
+                "itemGoods" : "스누피",
+                "usage" : "빵모자",
+                "content" :"",
+                "image" : "goods2/goods12.jpg"
+            },
+        ]
+    },
+    {
+        harryPotter: [
+            {
+                "store" : "와이제이커 마술용품점",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "망토(그리핀도르™)",
+                "content" :"",
+                "image" : "goods3/goods1.jpg"
+            },
+            {
+                "store" : "부엉이 우체국™ & 부엉이 방",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "손목 인형",
+                "content" :"",
+                "image" : "goods3/goods2.jpg"
+            },
+            {
+                "store" : "종코™의 '장난감 전문점'",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "늘어나는 귀",
+                "content" :"",
+                "image" : "goods3/goods3.jpg"
+            },
+            {
+                "store" : "와이제이커 마술용품점",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "넥타이(그리핀도르™)",
+                "content" :"",
+                "image" : "goods3/goods4.jpg"
+            },
+            {
+                "store" : "올리밴더스의 가게™",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "매지컬 완드(해리 포터™)",
+                "content" :"",
+                "image" : "goods3/goods5.jpg"
+            },
+            {
+                "store" : "올리밴더스의 가게™",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "매지컬 완드(헤르미온느 그레인저™)",
+                "content" :"",
+                "image" : "goods3/goods6.jpg"
+            },
+            {
+                "store" : "필치의 몰수품점™ 등",
+                "itemGoods" : "위저딩 월드 오브 해리 포터",
+                "usage" : "펜 세트(5개)",
+                "content" :"",
+                "image" : "goods3/goods7.jpg"
+            },
+            {
+                "store" : "글래드래그스 마법사 옷가게",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "머리띠",
+                "content" :"",
+                "image" : "goods3/goods8.jpg"
+            },
+            {
+                "store" : "필치의 몰수품점™ 등",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "사탕",
+                "content" :"",
+                "image" : "goods3/goods9.jpg"
+            },
+            {
+                "store" : "부엉이 우체국™ & 부엉이 방",
+                "itemGoods" : "위저딩 월드 오브 해리 포터™",
+                "usage" : "봉제인형 펜 케이스",
+                "content" :"",
+                "image" : "goods3/goods10.jpg"
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+        ]
+    },
+    {
+        sesame : [
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+        ]
+    },
+    {
+        spiderman : [
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+        ]
+    },
+    {
+        jurassicPark : [
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+            {
+                "store" : "",
+                "itemGoods" : "",
+                "usage" : "",
+                "content" :""
+            },
+        ]
+    },
 ]
