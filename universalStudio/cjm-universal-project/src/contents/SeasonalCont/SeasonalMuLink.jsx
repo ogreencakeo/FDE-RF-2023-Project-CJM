f2d97d1ac00c16f40af5cc3d3783dd665da9e136
ogreencakeo <magente2000@gmail.com>
2023-11-16 12:35:16 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/SeasonalCont/SeasonalMuLink.jsx
+++ universalStudio/cjm-universal-project/src/contents/SeasonalCont/SeasonalMuLink.jsx
@@ -1,42 +1,27 @@
-import { seasonalMenu } from "../../data/seasonalMenu"
-export function SeasonalMuLink(){
+import { seasonalMenu } from "../../data/seasonalMenu";
+export function SeasonalMuLink() {
     // const seasonMenu = seasonalMenu;
     console.log(seasonalMenu);
-    return seasonalMenu.map((v, i) =>
-        <div class="seasonal_menu_wrap" key={i}>
-            <div class="seasonal_menu_bx">
-                <div class="seasonal_menu_tit">
-                    <h1>시즈널 메뉴</h1>
-                    <div></div>
-                </div>
-                <ul>
-                    {
-                        <li>
-                            <a href="#">
-                                <div class="menu_block">
-                                    <span>{v.idx}</span>
-                                    <div>
-                                        <span>{v.location}</span>
-                                        {/* <!-- 이미지 --> */}
-                                    </div>
-                                    <div>
-                                        <i class="fa-solid fa-square-up-right"></i>
-                                    </div>
+    return seasonalMenu.map((v, i) => (
+        <>
+            <ul>
+                {
+                    <li key={i}>
+                        <a href="#">
+                            <div class="menu_block">
+                                <span>{v.idx}</span>
+                                <div>
+                                    <span>{v.location}</span>
+                                    {/* <!-- 이미지 --> */}
                                 </div>
-                            </a>
-                        </li>
-                    }
-                    
-                    {/* <li><a href="#"><span>피네간즈 바&그릴</span></a></li>
-                    <li><a href="#"><span>루이즈 N.Y. 피자 팔러</span></a></li>
-                    <li><a href="#"><span>멜즈 드라이브인</span></a></li>
-                    <li><a href="#"><span>스리 브룸스틱스™</span></a></li>
-                    <li><a href="#"><span>스누피™ 백롯 카페</span></a></li>
-                    <li><a href="#"><span>해피니스 카페®</span></a></li>
-                    <li><a href="#"><span>파크 사이드 그릴</span></a></li> */}
-
-                </ul>
-            </div>
-        </div>
-    )
+                                <div>
+                                    <i class="fa-solid fa-square-up-right"></i>
+                                </div>
+                            </div>
+                        </a>
+                    </li>
+                }
+            </ul>
+        </>
+    ));
 }
--- universalStudio/cjm-universal-project/src/contents/SeasonalMenu.jsx
+++ universalStudio/cjm-universal-project/src/contents/SeasonalMenu.jsx
@@ -8,7 +8,15 @@ export function SeasonalMenu() {
             {/*  추천 시즈널 메뉴 대특집  */}
             <RecommSeasonal />
             {/*시즈널 메뉴 링크 */}
-            <SeasonalMuLink />
+            <div class="seasonal_menu_wrap">
+                <div class="seasonal_menu_bx">
+                    <div class="seasonal_menu_tit">
+                        <h1>시즈널 메뉴</h1>
+                        <div></div>
+                    </div>
+                    <SeasonalMuLink />
+                </div>
+            </div>
         </>
     );
 }
