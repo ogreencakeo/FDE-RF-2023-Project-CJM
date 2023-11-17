50c3cbb868bbee762923ac0407e01e11dc2e67ad
ogreencakeo <magente2000@gmail.com>
2023-11-17 16:48:12 +0900

    sdf

--- universalStudio/cjm-universal-project/src/contents/Goods.jsx
+++ universalStudio/cjm-universal-project/src/contents/Goods.jsx
@@ -1,4 +1,3 @@
-
 // export function Goods(){
 //     return(
 //         <div className="goods_wrap">
@@ -6,39 +5,43 @@
 //         </div>
 //     )
 // }
-import React from 'react';
+import React from "react";
 import { Link, Route, Routes } from "react-router-dom";
-import { Supermario } from './GoodsCont/Supermario';
+import { Supermario } from "./GoodsCont/Supermario";
 import { Snoopy } from "./GoodsCont/Snoopy";
-import { HarryPotter } from './GoodsCont/HarryPotter';
-import { Spiderman } from './GoodsCont/Spiderman';
-import { Sesame } from './GoodsCont/Sesame';
-import { JurassicPark } from './GoodsCont/JurassicPark';
-import { TopArea } from '../layout/TopArea';
-import { FooterArea } from '../layout/FooterArea';
+import { HarryPotter } from "./GoodsCont/HarryPotter";
+import { Spiderman } from "./GoodsCont/Spiderman";
+import { Sesame } from "./GoodsCont/Sesame";
+import { JurassicPark } from "./GoodsCont/JurassicPark";
+import { TopArea } from "../layout/TopArea";
+import { FooterArea } from "../layout/FooterArea";
 
-import '../css/goods.css';
-import { GoodsMenu } from './module/GoodsMenu';
+import "../css/goods.css";
+import { GoodsMenu } from "./module/GoodsMenu";
+import { GoodsTopCont } from "./module/GoodsTopCont";
 
 export function Goods() {
     return (
-    <>
-        <div className="goods_wrap">
-            <div className="goods_menu">
-                <GoodsMenu />
+        <>
+            <div className="goods_wrap">
+                <div class="goods-top-cont-wrap">
+                    <GoodsTopCont />
+                </div>
+                <div className="goods_menu">
+                    <GoodsMenu />
+                </div>
+                <div className="goods_bx_wrap">
+                    <Routes>
+                        <Route index element={<Supermario />} />
+                        <Route path="supermario" element={<Supermario />} />
+                        <Route path="snoopy" element={<Snoopy />} />
+                        <Route path="harryPotter" element={<HarryPotter />} />
+                        <Route path="sesame" element={<Sesame />} />
+                        <Route path="spiderman" element={<Spiderman />} />
+                        <Route path="jurassicPark" element={<JurassicPark />} />
+                    </Routes>
+                </div>
             </div>
-            <div className="goods_bx_wrap">
-                <Routes>
-                    <Route index element={<Supermario />} />
-                    <Route path='supermario' element={<Supermario />} />
-                    <Route path='snoopy' element={<Snoopy />} />
-                    <Route path='harryPotter' element={<HarryPotter />} />
-                    <Route path='sesame' element={<Sesame />} />
-                    <Route path='spiderman' element={<Spiderman />} />
-                    <Route path='jurassicPark' element={<JurassicPark />} />
-                </Routes>
-            </div>
-        </div>
-    </>
+        </>
     );
 }
