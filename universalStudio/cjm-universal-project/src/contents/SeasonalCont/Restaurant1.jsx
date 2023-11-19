9de5ae6c7e5690d5a77824b70d6ae816984dec9f
jimin0427 <cjm0427@hs.ac.kr>
2023-11-19 23:29:12 +0900

    Update Restaurant1.jsx

--- universalStudio/cjm-universal-project/src/contents/SeasonalCont/Restaurant1.jsx
+++ universalStudio/cjm-universal-project/src/contents/SeasonalCont/Restaurant1.jsx
@@ -1,10 +1,11 @@
 import React from "react";
-import { Layout } from "../../layout/Layout";
+// import { Layout } from "../../layout/Layout";
 
 export function Restaurant1(){
     return(
-        <Layout>
-            sasdf
-        </Layout>
+        <>
+            <h1>fsdf</h1>
+            <h2>fsd</h2>
+        </>
     )
 }
