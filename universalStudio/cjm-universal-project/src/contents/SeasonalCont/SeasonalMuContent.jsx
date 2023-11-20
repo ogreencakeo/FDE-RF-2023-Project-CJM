636ec29b27f54bbfb03c1bfdcaff1f55801fa239
ogreencakeo <magente2000@gmail.com>
2023-11-20 18:18:28 +0900

    dsadsa

--- universalStudio/cjm-universal-project/src/contents/SeasonalCont/SeasonalMuContent.jsx
+++ universalStudio/cjm-universal-project/src/contents/SeasonalCont/SeasonalMuContent.jsx
@@ -2,17 +2,26 @@ export function SeasonalMuContent() {
     return (
         <div className="seasonal-main-content-wrap">
             <div className="seasonal-main-content-bx">
-                <h1>기간 한정! 크리스마스 프리미엄 코스</h1>
-                <h2>
-                    본고장 N.Y.의 스테이크 하우스에서 맛볼 수 있는 초호화 크리스마스 코스와 함께 추억의 한 페이지를
-                    장식할 특별한 하루를 보내세요.
-                </h2>
-                <img src="../../../images/seasonal/main/seasonal_main_img1.jpg" alt="사진" />
-                <p>
-                    가리비 구이와 대구의 브랑다드 그뤼에르 치즈 연어 구이와 시금치 리소토 스파이스향 홀란데이즈 소스
-                    앵거스 에이징 스테이크(숙성육) 트러플 버터 크로켓 피스타치오와 헤이즐넛의 가토 믹스베리&바닐라
-                    아이스크림 빵 커피 또는 홍차
-                </p>
+                <h1 className="seasonal-title">기간 한정! 크리스마스 프리미엄 코스<br/>겨울철의 맛있는 먹거리가 한가득!</h1>
+                <h2 className="seasonal-sub-title">#Christmas warm food</h2>
+                <div className="seasonal-img-bx">
+                    <div>
+                        <img src="../../../images/seasonal/main/seasonal_main_img1.jpg" alt="사진" />
+                        <span></span>
+                    </div>
+                    <div>
+                        <img src="../../../images/seasonal/main/seasonal_main_img2.jpg" alt="사진" />
+                        <span></span>
+                    </div>
+                    <div>
+                        <img src="../../../images/seasonal/main/seasonal_main_img3.jpg" alt="사진" />
+                        <span></span>
+                    </div>
+                    <div>
+                        <img src="../../../images/seasonal/main/seasonal_main_img4.jpg" alt="사진" />
+                        <span></span>
+                    </div>
+                </div>
             </div>
         </div>
     );
