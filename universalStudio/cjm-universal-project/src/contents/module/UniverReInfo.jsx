6724fafc1bb159f0b1db13ef30ba11927efa8be5
ogreencakeo <magente2000@gmail.com>
2023-11-24 14:32:33 +0900

    Update UniverReInfo.jsx

--- universalStudio/cjm-universal-project/src/contents/module/UniverReInfo.jsx
+++ universalStudio/cjm-universal-project/src/contents/module/UniverReInfo.jsx
@@ -4,7 +4,7 @@ import "../../css/universal_recomm.css";
 // 폰트어썸
 import { faCircleCheck } from "@fortawesome/free-regular-svg-icons";
 // import { faEarthAsia } from "@fortawesome/free-light-svg-icons";
-import { faEarthAsia } from "@fortawesome/free-solid-svg-icons";
+import { faEarthAsia, faSunPlantWilt } from "@fortawesome/free-solid-svg-icons";
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 
 {/* <span className="faEarthAsia" style={{ color: props.color }}> */}
@@ -21,8 +21,9 @@ export function UniverReInfo(props) {
                     <img src={`../../images/seasonal/character/character3.png`} alt="" /> : ""
                 }
             </div>
-            {/* <div className="universal_re_main_tit" style={{ backgroundColor: props.color }}>{props.category == "food" ? "푸드&레스토랑" : ""}</div><br />
+            <div className="universal_re_main_tit" style={{ backgroundColor: props.color }}>{props.category == "food" ? "푸드&레스토랑" : ""}</div><br />
             <div className="universal_re_main_tit2" style={{ backgroundColor: props.color }}>UNIVERSAL STUDIO만의 추천정보</div>
+            <FontAwesomeIcon icon={faSunPlantWilt} />
             <div className="universal_recommend_info_bx">
                 <table>
                     <tbody>
@@ -43,7 +44,7 @@ export function UniverReInfo(props) {
                     </tbody>
                 </table>
                 <div className="universal_recomm_img"></div>
-            </div> */}
+            </div>
         </div>
     );
 }
