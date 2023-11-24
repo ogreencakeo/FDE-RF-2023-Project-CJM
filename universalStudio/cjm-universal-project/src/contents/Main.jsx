b1ba601bec733f16545eaf55289f213dc8c34beb
ogreencakeo <magente2000@gmail.com>
2023-11-24 16:44:02 +0900

    sdf

--- universalStudio/cjm-universal-project/src/contents/Main.jsx
+++ universalStudio/cjm-universal-project/src/contents/Main.jsx
@@ -1,22 +1,32 @@
-import React, { useEffect } from 'react';
-import { faSquareInstagram, faSquareFacebook, faSquareTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
-import { faMapMarkerAlt, faClock, faSquareParking, faCar } from "@fortawesome/free-solid-svg-icons";
+import React, { useEffect } from "react";
+
+// 폰트어썸
 import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { faSquareInstagram, faSquareFacebook, faSquareTwitter, faYoutube } from "@fortawesome/free-brands-svg-icons";
+import { faMapMarkerAlt, faClock, faSquareParking, faCar, faGifts } from "@fortawesome/free-solid-svg-icons";
 
+import { pickUpData } from "../data/main/pickup.js";
 
-import { Link } from 'react-router-dom';
-import WaveText from './module/WaveText.jsx';
+import { Link } from "react-router-dom";
+
+import WaveText from "./module/WaveText.jsx";
+
+// 제이쿼리
+import $ from "jquery";
 
 // 다른 import 문은 여기에 추가하세요.
 
 export function Main() {
     useEffect(() => {
-        const mainImgStage = document.querySelector('.header-main-img-wrap');
+        const mainImgStage = document.querySelector(".header-main-img-wrap");
         setTimeout(() => {
-            mainImgStage.classList.add('on');
+            mainImgStage.classList.add("on");
         }, 400);
     }, []);
 
+    const rotateAni = () => {
+        $(".pick-up-img").toggleClass("on");
+    };
 
     return (
         <>
@@ -30,7 +40,7 @@ export function Main() {
                                 <div className="header-main-img"></div>
                             </div>
                             {/* Main Content */}
-                            <WaveText /> 
+                            <WaveText />
                             <div className="universal_link">
                                 <a href="https://www.usj.co.jp/web/ko/kr" target="_blank">
                                     GO TO SITE
@@ -39,23 +49,43 @@ export function Main() {
                             {/* Social Media */}
                             <div className="universal_sns">
                                 <div className="universal_instagram">
-                                    <Link to="https://www.instagram.com/universal_studios_japan/?ref=badge" target="_blank" title="인스타그램 바로가기">
-                                        <FontAwesomeIcon className="fa-square-instagra fa-brands" icon={faSquareInstagram} />
+                                    <Link
+                                        to="https://www.instagram.com/universal_studios_japan/?ref=badge"
+                                        target="_blank"
+                                        title="인스타그램 바로가기"
+                                    >
+                                        <FontAwesomeIcon
+                                            className="fa-square-instagra fa-brands"
+                                            icon={faSquareInstagram}
+                                        />
                                     </Link>
                                 </div>
                                 <div className="universal_facebook">
-                                    <Link to="https://www.facebook.com/UniversalStudiosJapan" target="_blank" title="인페이스북 바로가기">
-                                        <FontAwesomeIcon className="fa-brands fa-square-facebook" icon={faSquareFacebook} />
+                                    <Link
+                                        to="https://www.facebook.com/UniversalStudiosJapan"
+                                        target="_blank"
+                                        title="인페이스북 바로가기"
+                                    >
+                                        <FontAwesomeIcon
+                                            className="fa-brands fa-square-facebook"
+                                            icon={faSquareFacebook}
+                                        />
                                     </Link>
                                 </div>
                                 <div className="universal_twitter">
                                     <Link to="https://twitter.com/USJ_Official" target="_blank" title="트위터 바로가기">
-                                        <FontAwesomeIcon className="fa-brands fa-square-twitter" icon={faSquareTwitter} />
+                                        <FontAwesomeIcon
+                                            className="fa-brands fa-square-twitter"
+                                            icon={faSquareTwitter}
+                                        />
                                     </Link>
-
                                 </div>
                                 <div className="universal_youtube">
-                                    <Link to="https://www.youtube.com/user/usjTV" target="_blank" title="유튜브 바로가기">
+                                    <Link
+                                        to="https://www.youtube.com/user/usjTV"
+                                        target="_blank"
+                                        title="유튜브 바로가기"
+                                    >
                                         <FontAwesomeIcon className="fa-brands fa-youtube" icon={faYoutube} />
                                     </Link>
                                 </div>
@@ -70,25 +100,25 @@ export function Main() {
                 <div className="utility-panel">
                     <a href="#">
                         <div className="panel-clock">
-                            <FontAwesomeIcon className='panel-icon' icon={faClock} />
+                            <FontAwesomeIcon className="panel-icon" icon={faClock} />
                             <span>영업시간</span>
                         </div>
                     </a>
                     <a href="#">
                         <div className="panel-park-map">
-                            <FontAwesomeIcon className='panel-icon' icon={faMapMarkerAlt} />
+                            <FontAwesomeIcon className="panel-icon" icon={faMapMarkerAlt} />
                             <span>파크맵</span>
                         </div>
                     </a>
                     <a href="">
                         <div className="panel-clock">
-                            <FontAwesomeIcon className='panel-icon' icon={faSquareParking} />
+                            <FontAwesomeIcon className="panel-icon" icon={faSquareParking} />
                             <span>주차장 정보</span>
                         </div>
                     </a>
                     <a href="#">
                         <div className="panel-clock">
-                            <FontAwesomeIcon className='panel-icon' icon={faCar} />
+                            <FontAwesomeIcon className="panel-icon" icon={faCar} />
                             <span>교통편</span>
                         </div>
                     </a>
@@ -98,21 +128,22 @@ export function Main() {
             {/* Pick-up Section */}
             <div className="pick-up">
                 <div className="pick-up-wrap">
-                    <div className="pick-up-img-box">
-                        <div className="pick-up-img">
-                            <img className="pick1" src="./images/main/pickup/pickup1.jpg" alt="" />
-                            <div className="pick2">
-                                <img src="./images/main/pickup/pickImg1.jpg" alt="" />
+                    <div className="pick-up-img-box" onClick={rotateAni}>
+                        {pickUpData.map((v, i) => (
+                            <div className="pick-up-img" key={i}>
+                                <div className="pick1">
+                                    <img src={`./images/main/pickup/${v.img}`} alt={`${v.idx}번째 이미지`} />
+                                    <h2>{v.title}</h2>
+                                    <span>
+
+                                        <FontAwesomeIcon icon={v.emotion} />
+                                    </span>
+                                </div>
+                                <div className="pick2">
+                                    <p>{v.cont}</p>
+                                </div>
                             </div>
-                        </div>
-                    </div>
-                    <div className="pick-up-img-box">
-                        <div className="pick-up-img">
-                            <img className="pick1" src="./images/main/pickup/pickImg2.jpg" alt="" />
-                            <div className="pick2">
-                                <img src="./images/main/pickup/pickup2.jpg" alt="" />
-                            </div>
-                        </div>
+                        ))}
                     </div>
                 </div>
             </div>
---
+++ universalStudio/cjm-universal-project/src/data/main/pickup.js
@@ -0,0 +1,9 @@
+export const pickUpData = [
+    {
+        "idx" : "1",
+        "title" : "NO LIMIT! 크리스마스",
+        "cont" : "개최기간：2023년11월 21일(화) ~ 2024년1월 8일(월)",
+        "emotion" : "faGifts",
+        "img" : "pickup1.jpg"
+    },
+];
