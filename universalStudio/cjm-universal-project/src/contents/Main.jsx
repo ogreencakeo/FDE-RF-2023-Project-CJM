9e1fe3be606c282b9018044a514d94c6cca45c1b
jimin0427 <cjm0427@hs.ac.kr>
2023-11-24 19:53:48 +0900

    SAD

--- universalStudio/cjm-universal-project/src/contents/Main.jsx
+++ universalStudio/cjm-universal-project/src/contents/Main.jsx
@@ -12,7 +12,8 @@ import {
     faSpider,
     faChessKing,
     faHatWizard,
-    faMountainSun
+    faMountainSun,
+    faEarthAsia
 } from "@fortawesome/free-solid-svg-icons";
 
 import { pickUpData } from "../data/main/pickup.js";
@@ -23,15 +24,40 @@ import WaveText from "./module/WaveText.jsx";
 
 // 제이쿼리
 import $ from "jquery";
+import { useState } from "react";
 
 // 다른 import 문은 여기에 추가하세요.
 
 export function Main() {
+
+    const load_text = 'UNIVERSAL STUDIO';
+
+    const [currentIdx, setCurrentIdx] = useState(0);
+    const [isVisible, setIsVisible] = useState(true);
+    const [isMainVisible, setIsMainVisible] = useState(false);
+
+    // UNIVERSAL STUDIO 글자 로딩 페이지
+    useEffect(() => {
+        const interval = setInterval(() => {
+            if (currentIdx < load_text.length) {
+                setCurrentIdx((prevIdx) => prevIdx + 1);
+            } else {
+                clearInterval(interval);
+                setTimeout(() => {
+                    setIsVisible(false);
+                    setTimeout(() => {
+                        setIsMainVisible(true);
+                    }, 1000)
+                }, 200);
+            }
+        }, 200);
+    })
+
     useEffect(() => {
         const mainImgStage = document.querySelector(".header-main-img-wrap");
         setTimeout(() => {
             mainImgStage.classList.add("on");
-        }, 400);
+        }, 1500);
     }, []);
 
     const rotateAni = (e) => {
@@ -61,122 +87,138 @@ export function Main() {
 
     return (
         <>
-            {/* Header */}
-            <div className="header">
-                <section>
-                    <div className="header-img">
-                        <div className="header-img-wrap">
-                            {/* Main Image */}
-                            <div className="header-main-img-wrap">
-                                <div className="header-main-img"></div>
-                            </div>
-                            {/* Main Content */}
-                            <WaveText />
-                            <div className="universal_link">
-                                <a href="https://www.usj.co.jp/web/ko/kr" target="_blank">
-                                    GO TO SITE
-                                </a>
-                            </div>
-                            {/* Social Media */}
-                            <div className="universal_sns">
-                                <div className="universal_instagram">
-                                    <Link
-                                        to="https://www.instagram.com/universal_studios_japan/?ref=badge"
-                                        target="_blank"
-                                        title="인스타그램 바로가기"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="fa-square-instagra fa-brands"
-                                            icon={faSquareInstagram}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="universal_facebook">
-                                    <Link
-                                        to="https://www.facebook.com/UniversalStudiosJapan"
-                                        target="_blank"
-                                        title="인페이스북 바로가기"
-                                    >
-                                        <FontAwesomeIcon
-                                            className="fa-brands fa-square-facebook"
-                                            icon={faSquareFacebook}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="universal_twitter">
-                                    <Link to="https://twitter.com/USJ_Official" target="_blank" title="트위터 바로가기">
-                                        <FontAwesomeIcon
-                                            className="fa-brands fa-square-twitter"
-                                            icon={faSquareTwitter}
-                                        />
-                                    </Link>
-                                </div>
-                                <div className="universal_youtube">
-                                    <Link
-                                        to="https://www.youtube.com/user/usjTV"
-                                        target="_blank"
-                                        title="유튜브 바로가기"
-                                    >
-                                        <FontAwesomeIcon className="fa-brands fa-youtube" icon={faYoutube} />
-                                    </Link>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                </section>
-            </div>
+            {/* 로딩 구역 */}
+            <div className={`loading-page ${isVisible ? 'visible' : 'hidden'}`}>
+                <div>
+                    <FontAwesomeIcon icon={faEarthAsia} />
+                </div>
+                <div className="load-text">
 
-            {/* Main Section 1 */}
-            <div className="main1">
-                <div className="utility-panel">
-                    <a href="#">
-                        <div className="panel-clock">
-                            <FontAwesomeIcon className="panel-icon" icon={faClock} />
-                            <span>영업시간</span>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div className="panel-park-map">
-                            <FontAwesomeIcon className="panel-icon" icon={faMapMarkerAlt} />
-                            <span>파크맵</span>
-                        </div>
-                    </a>
-                    <a href="">
-                        <div className="panel-clock">
-                            <FontAwesomeIcon className="panel-icon" icon={faSquareParking} />
-                            <span>주차장 정보</span>
-                        </div>
-                    </a>
-                    <a href="#">
-                        <div className="panel-clock">
-                            <FontAwesomeIcon className="panel-icon" icon={faCar} />
-                            <span>교통편</span>
-                        </div>
-                    </a>
+                    {load_text.split('').map((v, i) =>
+                        <span key={i} style={{ animationDelay: `${i * 0.15}s` }}>
+                            {v}
+                        </span>
+                    )}
                 </div>
             </div>
-
-            {/* Pick-up Section */}
-            <div className="pick-up">
-                <div className="pick-up-wrap">
-                    {pickUpData.map((v, i) => (
-                        <div className="pick-up-img-box" onClick={rotateAni} key={i}>
-                            <div className="pick-up-img">
-                                <div className="pick1">
-                                    <img src={`./images/main/pickup/${v.img}`} alt={`${v.idx}번째 이미지`} />
-                                    <h2>{v.title}</h2>
-                                    <span>
-                                        {/* <FontAwesomeIcon icon={v.emotion} /> */}
-                                        {/* {Object.keys(result).find(pickUpData[i][v.name]) && result[v.name]} */}
-                                    </span>
+            {/* Header */}
+            <div className={`${isMainVisible ? 'visible' : 'hidden'}`}>
+                <div className="header">
+                    <section>
+                        <div className="header-img">
+                            <div className="header-img-wrap">
+                                {/* Main Image */}
+                                <div className="header-main-img-wrap">
+                                    <div className="header-main-img"></div>
                                 </div>
-                                <div className="pick2">
-                                    {Object.keys(result).indexOf(pickUpData[i]["emotion"]) != -1 && result[v.emotion]}
-                                    <p>{v.cont}</p>
+                                {/* Main Content */}
+                                <WaveText />
+                                <div className="universal_link">
+                                    <a href="https://www.usj.co.jp/web/ko/kr" target="_blank">
+                                        GO TO SITE
+                                    </a>
+                                </div>
+                                {/* Social Media */}
+                                <div className="universal_sns">
+                                    <div className="universal_instagram">
+                                        <Link
+                                            to="https://www.instagram.com/universal_studios_japan/?ref=badge"
+                                            target="_blank"
+                                            title="인스타그램 바로가기"
+                                        >
+                                            <FontAwesomeIcon
+                                                className="fa-square-instagra fa-brands"
+                                                icon={faSquareInstagram}
+                                            />
+                                        </Link>
+                                    </div>
+                                    <div className="universal_facebook">
+                                        <Link
+                                            to="https://www.facebook.com/UniversalStudiosJapan"
+                                            target="_blank"
+                                            title="인페이스북 바로가기"
+                                        >
+                                            <FontAwesomeIcon
+                                                className="fa-brands fa-square-facebook"
+                                                icon={faSquareFacebook}
+                                            />
+                                        </Link>
+                                    </div>
+                                    <div className="universal_twitter">
+                                        <Link to="https://twitter.com/USJ_Official" target="_blank" title="트위터 바로가기">
+                                            <FontAwesomeIcon
+                                                className="fa-brands fa-square-twitter"
+                                                icon={faSquareTwitter}
+                                            />
+                                        </Link>
+                                    </div>
+                                    <div className="universal_youtube">
+                                        <Link
+                                            to="https://www.youtube.com/user/usjTV"
+                                            target="_blank"
+                                            title="유튜브 바로가기"
+                                        >
+                                            <FontAwesomeIcon className="fa-brands fa-youtube" icon={faYoutube} />
+                                        </Link>
+                                    </div>
                                 </div>
                             </div>
                         </div>
-                    ))}
+                    </section>
+                </div>
+                {/* Main Section 1 */}
+                <div className="main1">
+                    <div className="utility-panel">
+                        <a href="#">
+                            <div className="panel-clock">
+                                <FontAwesomeIcon className="panel-icon" icon={faClock} />
+                                <span>영업시간</span>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className="panel-park-map">
+                                <FontAwesomeIcon className="panel-icon" icon={faMapMarkerAlt} />
+                                <span>파크맵</span>
+                            </div>
+                        </a>
+                        <a href="">
+                            <div className="panel-clock">
+                                <FontAwesomeIcon className="panel-icon" icon={faSquareParking} />
+                                <span>주차장 정보</span>
+                            </div>
+                        </a>
+                        <a href="#">
+                            <div className="panel-clock">
+                                <FontAwesomeIcon className="panel-icon" icon={faCar} />
+                                <span>교통편</span>
+                            </div>
+                        </a>
+                    </div>
+                </div>
+
+
+                {/* Pick-up Section */}
+                <div className="pick-up">
+                    <div className="pick-up-wrap">
+                        {pickUpData.map((v, i) => (
+                            <div className="pick-up-img-box" onClick={rotateAni} key={i}>
+                                <div className="pick-up-img">
+                                    <div className="pick1">
+                                        <img src={`./images/main/pickup/${v.img}`} alt={`${v.idx}번째 이미지`} />
+                                        <h2>{v.title}</h2>
+                                        <span>
+                                            {/* <FontAwesomeIcon icon={v.emotion} /> */}
+                                            {/* {Object.keys(result).find(pickUpData[i][v.name]) && result[v.name]} */}
+                                        </span>
+                                    </div>
+                                    <div className="pick2">
+                                        {Object.keys(result).indexOf(pickUpData[i]["emotion"]) != -1 && result[v.emotion]}
+                                        <p>{v.cont}</p>
+                                    </div>
+                                </div>
+                            </div>
+                        ))}
+                    </div>
                 </div>
             </div>
 
