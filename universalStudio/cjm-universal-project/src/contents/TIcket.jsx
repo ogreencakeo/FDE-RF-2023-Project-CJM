dfa876cf3c95deffb6a08551a637ba0202e8d75b
ogreencakeo <magente2000@gmail.com>
2023-11-27 17:08:07 +0900

    gh

--- universalStudio/cjm-universal-project/src/contents/TIcket.jsx
+++ universalStudio/cjm-universal-project/src/contents/TIcket.jsx
@@ -1,6 +1,29 @@
+// 폰트어썸
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import {
+    faShareFromSquare,
+    faPaste,
+    faCircleUser,
+    faHourglassHalf,
+    faCircleXmark,
+} from "@fortawesome/free-regular-svg-icons";
+import { faCoins, faLocationCrosshairs, faCalendarWeek } from "@fortawesome/free-solid-svg-icons";
 import "../css/ticket.css";
 
 export function Ticket() {
+    const makeTicImgList = () => {
+        let temp = [];
+        for (let i = 0; i < 4; i++) {
+            temp[x] = (
+                <div>
+                    <li key={i}>
+                        <img src={`./images/slide-main${i + 1}.jpg`} alt="슬라이드 이미지" />
+                    </li>
+                </div>
+            );
+            return temp;
+        }
+    };
     return (
         <>
             <div class="ticket-wrap">
@@ -19,7 +42,12 @@ export function Ticket() {
                         {/* 티켓 제목및 컨텐츠 (왼쪽) */}
                         <div class="ticket-content-wrap-left">
                             <div class="ticket-wrap-img">
-                                <img src="./images/slide-main2.jpg" alt="" />
+                                <div>
+                                    <img src="./images/slide-main1.jpg" alt="이미지" />
+                                </div>
+                                <div>
+                                    <ul>{makeTicImgList}</ul>
+                                </div>
                             </div>
                         </div>
                         {/* 티켓 제목및 컨텐츠 (오른쪽) */}
@@ -29,16 +57,17 @@ export function Ticket() {
                                 <h2>테마파크&워터파크</h2>
                                 <div class="ticket-tit-cont-top">
                                     <h1>유니버셜 스튜디오 재팬 1일 입장권</h1>
-                                    <i class="fa-regular fa-share-from-square"></i>
+                                    <FontAwesomeIcon icon={faShareFromSquare} />
                                 </div>
                                 {/* 티켓 컨텐츠및 제목 하단 */}
                                 <div class="ticket-tit-cont-bottom">
                                     <div class="ticket-tit-money">
-                                        <i class="fa-solid fa-coins"></i>
+                                        <FontAwesomeIcon icon={faCoins} />
                                         <span>48,300부터</span>
                                     </div>
                                     <div class="ticket-tit-where">
                                         <i class="fa-solid fa-location-crosshairs"></i>
+                                        <FontAwesomeIcon icon={faLocationCrosshairs} />
                                         <span>오사카</span>
                                     </div>
                                 </div>
@@ -47,30 +76,40 @@ export function Ticket() {
                                     <h1>상품정보</h1>
                                     <div class="ticket-info-bx">
                                         <div>
-                                            <i class="fa-regular fa-paste"></i>
+                                            <span>
+                                                <FontAwesomeIcon icon={faPaste} />
+                                            </span>
                                             <span>바우처 즉시 제공</span>
                                         </div>
                                         <div>
-                                            <i class="fa-regular fa-user"></i>
+                                            <span>
+                                                <FontAwesomeIcon icon={faCircleUser} />
+                                            </span>
                                             <span>1명</span>
                                         </div>
                                         <div>
-                                            <i class="fa-solid fa-calendar-week"></i>
+                                            <span>
+                                                <FontAwesomeIcon icon={faCalendarWeek} />
+                                            </span>
                                             <span>모바일 바우처 사용</span>
                                         </div>
                                         <div>
-                                            <i class="fa-solid fa-hourglass-start"></i>
+                                            <span>
+                                                <FontAwesomeIcon icon={faHourglassHalf} />
+                                            </span>
                                             <span>유효기간내 사용</span>
                                         </div>
                                         <div>
-                                            <i class="fa-solid fa-user-xmark"></i>
+                                            <span>
+                                                <FontAwesomeIcon icon={faCircleXmark} />
+                                            </span>
                                             <span>취소불가</span>
                                         </div>
                                     </div>
                                 </div>
                                 <div class="ticket-reviewer-cont-wrap">
                                     <div class="ticket-reviewer-img">
-                                        <img src="./images/travelers-choice.jpg" alt="리뷰어 선택" />
+                                        <img src="../images/travelers-choice.jpg" alt="리뷰어 선택" />
                                     </div>
                                     <div class="ticket-reviewer-cont">
                                         <h1>전세계 트립 어드바이저 리뷰어들이 선택!</h1>
