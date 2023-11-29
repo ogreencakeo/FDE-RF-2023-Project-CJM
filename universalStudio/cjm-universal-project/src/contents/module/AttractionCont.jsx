a15b6a914e328501121108e15d2675d1a42f670a
ogreencakeo <magente2000@gmail.com>
2023-11-29 18:15:05 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/module/AttractionCont.jsx
+++ universalStudio/cjm-universal-project/src/contents/module/AttractionCont.jsx
@@ -1,10 +1,31 @@
 
+import { Link } from 'react-router-dom';
 import '../../css/attractionCont.css';
+import {attractionData} from '../../data/attraction/attractionData';
 export function AttractionCont(){
+    const selData = attractionData;
     return(
         <>
             <div className="attractionCont-list-wrap">
-                
+                {
+                    selData.map((v, i) => 
+                        <Link to='/detail' state={{
+                            name : v.name,
+                            logo : v.logo,
+                            img : v.img,
+                            map : v.map,
+                            title : v.title,
+                            desc : v.desc
+                        }}>
+                            <div className='attractionCont-list-bx' key={i}>
+                                <div className="attractionCont-list-img">
+                                    <img src={`../images/attraction/${v.img}`} alt={v.name} />
+                                </div>
+                                <h2>{v.name}</h2>
+                            </div>
+                        </Link>
+                    )
+                }
             </div>
         </>
     )
