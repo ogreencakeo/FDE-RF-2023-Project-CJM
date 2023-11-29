b24b39b942096250b0137ae95bddb958861f11c7
ogreencakeo <magente2000@gmail.com>
2023-11-29 16:55:34 +0900

    sdf

--- universalStudio/cjm-universal-project/src/data/attraction/attractionData.js
+++ universalStudio/cjm-universal-project/src/data/attraction/attractionData.js
@@ -14,8 +14,89 @@ export const attractionData = [
         map : "map/attraction1.jpg",
         name : "마리오 카트: 쿠파의 도전장™",
         title : "마리오 카트를 타고 레이스!",
-        desc : "그 유명한 마리오 카트의 세계와 놀라움 가득한 갖가지 코스가 바로 눈앞에 등장한다! 등껍질을 던져 적을 격퇴하면서 마리오 그리고 피치공주와 함께 돌진하라! 파크만의 최신 기술로 실현되는 세계 최초*의 마리오 카트 체험에 두근두근 흥분이 멈추지 않는다!^* 마리오 카트 및 마리오의 게임 속 세계를 테마로 한 테마파크의 오리지널 어트랙션, 게임을 취급하는 시설을 대상으로 한 자사 조사."
-    }
+        desc : "그 유명한 마리오 카트의 세계와 놀라움 가득한 갖가지 코스가 바로 눈앞에 등장한다! 등껍질을 던져 적을 격퇴하면서 마리오 그리고 피치공주와 함께 돌진하라! 파크만의 최신 기술로 실현되는 세계 최초*의 마리오 카트 체험에 두근두근 흥분이 멈추지 않는다!^* 마리오 카트 및 마리오의 게임 속 세계를 테마로 한 테마파크의 오리지널 어트랙션, 게임을 취급하는 시설을 대상으로 한 자사 조사.",
+        type : 'nintendo'
+    },
+    {
+        idx : "2",
+        logo : "logo/attraction2.jpg",
+        img : "img/attraction2.jpg",
+        map : "map/attraction2.jpg",
+        name : "만나기",
+        title : "마리오와 피치공주 등을 만나러 가자!",
+        desc : "마리오와 루이지, 피치공주가 말을 걸어온다!^해맑은 키노피오와 만나서 기념 촬영도 할 수 있어요.^파워 업 밴드™를 손에 차면 즐거움이 더욱 커질지도.",
+        type : 'nintendo'
+    },
+    {
+        idx : "3",
+        logo : "logo/attraction3.png",
+        img : "img/attraction3.jpg",
+        map : "map/attraction3.jpg",
+        name : "요시 어드벤처™",
+        title : "요시와 모험 여행을 떠나자!",
+        desc : "요시의 등에 타고 키노피오대장을 쫓아서 보물찾기 모험을 떠나자!^ 대장이 깜빡하고 놓고 간 지도를 참고하여 사방에 숨어 있는 3개의 달걀을 찾아내자.^ 키다리산에서 버섯 왕국을 내려다보기도 하고 깜찍한 아이들과 만나는 등 즐거움이 한가득!",
+        type : 'nintendo'
+    },
+    {
+        idx : "3",
+        logo : "logo/attraction3.png",
+        img : "img/attraction3.jpg",
+        map : "map/attraction3.jpg",
+        name : "요시 어드벤처™",
+        title : "요시와 모험 여행을 떠나자!",
+        desc : "요시의 등에 타고 키노피오대장을 쫓아서 보물찾기 모험을 떠나자!^ 대장이 깜빡하고 놓고 간 지도를 참고하여 사방에 숨어 있는 3개의 달걀을 찾아내자.^ 키다리산에서 버섯 왕국을 내려다보기도 하고 깜찍한 아이들과 만나는 등 즐거움이 한가득!",
+        type : 'nintendo'
+    },
+    {
+        idx : "4",
+        logo : "logo/attraction4.jpg",
+        img : "img/attraction4.jpg",
+        map : "map/attraction4.jpg",
+        name : "쿠파주니어가 훔쳐 간 골든버섯을 되찾아라!",
+        title : "요시와 모험 여행을 떠나자!",
+        desc : "골든버섯을 쿠파주니어에게 도둑맞았다! 곳곳에 있는 적들을 쓰러뜨리고 3개의 열쇠를 획득하라! 열쇠를 다 획득한 후에 기다리고 있는 것은 쿠파주니어와의 대흥분 보스 배틀.^ 자 그럼 쿠파주니어를 무찌르고 무사히 골든버섯을 되찾을 수 있을까!?",
+        type : 'nintendo'
+    },
+    {
+        idx : "5",
+        logo : "logo/attraction5.jpg",
+        img : "img/attraction5.jpg",
+        map : "map/attraction5.jpg",
+        name : "올리밴더스의 가게™",
+        title : "‘지팡이가 마법사를 선택하는’ 상황을 체험할 수 있습니다.",
+        desc : "마법지팡이 상자가 천장까지 수북하게 쌓여 있는 먼지 낀 작은 가게 안에서 지팡이 파수꾼과 함께 ‘지팡이가 마법사를 선택하는’ 상황을 체험할 수 있습니다.",
+        type : 'harrypotter'
+    },
+    {
+        idx : "6",
+        logo : "logo/attraction5.jpg",
+        img : "img/attraction6.jpg",
+        map : "map/attraction6.jpg",
+        name : "완드 스터디",
+        title : "엄청난 마법이 눈앞에서 실제로 일어난다!",
+        desc : "지금껏 본 적 없는 엄청난 마법이 당신의 눈앞에서 실제로 일어납니다! 훌륭한 마법사가 되기 위해서 호그와트™ 마법 학교 네 곳의 기숙사 학생들은 마법 연습 중입니다.^ 성공과 실패를 거듭하면서 열심히 연습하는 학생들과 함께, 눈앞에서 실제로 일어나는 마법의 매력에 빠져 보세요. 용기 있는 마법사 여러분, 마법을 거는 학생들에게 도움을 주세요!",
+        type : 'harrypotter'
+    },
+    {
+        idx : "7",
+        logo : "logo/attraction5.jpg",
+        img : "img/attraction7.jpg",
+        map : "map/attraction7.jpg",
+        name : "지팡이 마법",
+        title : "마법을 걸자!",
+        desc : "마법사들이 사는 곳, 호그스미드 마을™에서는 당신도 실제로 마법을 걸 수 있어요. 그 유명한 창문과 석조 벽, 그리고 뒷골목에서도 특별한 마법을 걸 수 있어요!^주문을 외우며 지팡이를 휘둘러 보세요. 그러면 갑자기 주변에 눈이 흩날리거나 커다란 솥에서 물이 뿜어져 나와요! 처음에는 실패해도 몇 번이고 연습하다 보면 꼭 잘하게 될 거예요.",
+        type : 'harrypotter'
+    },
+    {
+        idx : "8",
+        logo : "logo/attraction5.jpg",
+        img : "img/attraction8.jpg",
+        map : "map/attraction8.jpg",
+        name : "트리위저드 스피릿 랠리",
+        title : "마법 학교 학생들의 퍼포먼스.",
+        desc : "트리위저드 시합을 위해 호그와트™ 마법 학교에 방문한 덤스트랭과 보바통 마법 학교의 퍼포먼스. 파워풀한 액션과 우아한 연기에 빠져드실 것입니다.",
+        type : 'harrypotter'
+    },
 ];
 
 /* 
