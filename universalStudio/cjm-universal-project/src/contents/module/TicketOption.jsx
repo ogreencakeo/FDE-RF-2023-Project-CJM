8d09c7e4eea394c737d5fbe0ddce5483b70ce055
ogreencakeo <magente2000@gmail.com>
2023-11-30 18:17:48 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/module/TicketOption.jsx
+++ universalStudio/cjm-universal-project/src/contents/module/TicketOption.jsx
@@ -2,7 +2,12 @@ import { ticketData } from "../../data/ticket/ticket_option";
 export function TicketOption(){
     return(
         <>
-            
+            {/* 티켓 서브 옵션 박스 */}
+            <div className="ticket-suboption-wrap">
+                <h1>[1일권 Low Price (A시즌)]</h1>
+                <h2>성인(만12 - 64세)</h2>
+                <h2>₩ 74,300</h2>
+            </div>
         </>
     );
 }
