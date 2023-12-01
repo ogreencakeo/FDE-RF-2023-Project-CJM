d8cb0f87a1b7d16d127353c06afd08752d66fc6f
ogreencakeo <magente2000@gmail.com>
2023-12-01 18:18:35 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/Member.jsx
+++ universalStudio/cjm-universal-project/src/contents/Member.jsx
@@ -17,39 +17,61 @@ export function Member(){
                     <h1><FontAwesomeIcon icon={faUserGroup} /></h1>
                     <h2>회원가입</h2>
                     <form action="process.php" method="post">
+                        <table>
+                            <tr>
+                                <td><label>아이디 </label></td>
+                                <td><input type="text" maxLength="20" placeholder="아이디를 입력해주세요" /></td>
+                            </tr>
+                            <tr>
+                                <td><label>비밀번호 </label></td>
+                                <td><input type="password" maxLength="20" placeholder="비밀번호를 입력해주세요" /></td>
+                            </tr>
+                            <tr>
+                                <td><label>비밀번호 확인 </label></td>
+                                <td><input type="password" maxLength="20" placeholder="비밀번호를 한번 더 입력해주세요" /></td>
+                            </tr>
+                            <tr>
+                                <td><label>닉네임</label></td>
+                                <td><input type="text" maxLength="20" placeholder="이름을 입력해주세요" /></td>
+                            </tr>
+                            <tr>
+                                <td><label>이메일</label></td>
+                                <td><input type="text" maxLength={50} placeholder="예)universal@studio.com" /></td>
+                            </tr>
+                            <tr cosp>
+                            <button className="sbtn">
+                                    가입하기
+                                </button>
+                            </tr>
+                            <tr> 로그인 페이지로 이동하겠습니까?
+                                <Link to='/login'>로그인하기</Link></tr>
+                        </table>
                         <ul>
-                            {/* 아이디 */}
                             <li>
                                 <label>아이디 </label>
                                 <input type="text" maxLength="20" placeholder="아이디를 입력해주세요" />
                             </li>
-                            {/* 비밀번호 */}
                             <li>
                                 <label>비밀번호 </label>
                                 <input type="password" maxLength="20" placeholder="비밀번호를 입력해주세요" />
                             </li>
-                            {/* 비밀번호 확인 */}
                             <li>
                                 <label>비밀번호 확인 </label>
                                 <input type="password" maxLength="20" placeholder="비밀번호를 한번 더 입력해주세요" />
                             </li>
-                            {/* 이름 */}
                             <li>
                                 <label>닉네임</label>
                                 <input type="text" maxLength="20" placeholder="이름을 입력해주세요" />
                             </li>
-                            {/* 이메일 */}
                             <li>
                                 <label>이메일</label>
                                 <input type="text" maxLength={50} placeholder="예)universal@studio.com" />
                             </li>
-                            {/* 버튼 */}
                             <li style={{overflow : 'hidden'}}>
                                 <button className="sbtn">
                                     가입하기
                                 </button>
                             </li>
-                            {/* 로그인 페이지 링크 */}
                             <li>
                                 로그인 페이지로 이동하겠습니까?
                                 <Link to='/login'>로그인하기</Link>
