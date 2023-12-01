5f6793c3812e53de746a8e7136b0fa400c9788ad
ogreencakeo <magente2000@gmail.com>
2023-12-01 18:09:42 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/Member.jsx
+++ universalStudio/cjm-universal-project/src/contents/Member.jsx
@@ -1,8 +1,20 @@
+import { Link } from "react-router-dom";
+
+// CSS
+import '../css/member.css';
+
+// 폰트어썸
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+
+import {faUserGroup} from "@fortawesome/free-solid-svg-icons";
+
 export function Member(){
     return(
         <>
             <div className="join-the-membership-wrap">
+                {/* 회원가입 박스 */}
                 <div className="member-bx">
+                    <h1><FontAwesomeIcon icon={faUserGroup} /></h1>
                     <h2>회원가입</h2>
                     <form action="process.php" method="post">
                         <ul>
@@ -24,7 +36,23 @@ export function Member(){
                             {/* 이름 */}
                             <li>
                                 <label>닉네임</label>
-                                <input type="text" />
+                                <input type="text" maxLength="20" placeholder="이름을 입력해주세요" />
+                            </li>
+                            {/* 이메일 */}
+                            <li>
+                                <label>이메일</label>
+                                <input type="text" maxLength={50} placeholder="예)universal@studio.com" />
+                            </li>
+                            {/* 버튼 */}
+                            <li style={{overflow : 'hidden'}}>
+                                <button className="sbtn">
+                                    가입하기
+                                </button>
+                            </li>
+                            {/* 로그인 페이지 링크 */}
+                            <li>
+                                로그인 페이지로 이동하겠습니까?
+                                <Link to='/login'>로그인하기</Link>
                             </li>
                         </ul>
                     </form>
