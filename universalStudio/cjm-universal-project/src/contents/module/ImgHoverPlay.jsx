7fd02840ed24a43c310b0667f60098d1d6cf92b9
jimin0427 <cjm0427@hs.ac.kr>
2023-12-03 18:39:38 +0900

    asd

--- universalStudio/cjm-universal-project/src/contents/module/ImgHoverPlay.jsx
+++ universalStudio/cjm-universal-project/src/contents/module/ImgHoverPlay.jsx
@@ -1,11 +1,89 @@
+import { useEffect, useState } from 'react';
 import '../../css/img_hover.css';
+import { UniverSalText } from './UniverSalText.jsx';
+// import '../../Function/imghoverplay.js';
 
-export function ImgHoverPlay(){
-    return(
+export function ImgHoverPlay() {
+
+    const [images, setImages] = useState([
+        './images/attraction/logo/attraction1.jpg',
+        './images/attraction/logo/attraction2.jpg',
+        './images/attraction/logo/attraction3.png',
+        './images/attraction/logo/attraction4.jpg',
+        './images/attraction/logo/attraction5.jpg',
+        './images/attraction/logo/attraction12.jpg',
+        './images/attraction/logo/attraction14.jpg',
+        './images/attraction/logo/attraction17.jpg',
+        './images/attraction/logo/attraction18.jpg',
+        './images/attraction/logo/attraction39.png',
+        './images/attraction/logo/attraction40.jpg',
+        './images/attraction/logo/attraction41.jpg',
+    ]);
+
+    const [canShowImage, setCanShowImage] = useState(true);
+
+    const handleMouseMove = (event) => {
+        if (canShowImage) {
+            const mouseX = event.pageX;  // clientX 대신에 pageX 사용
+            const mouseY = event.pageY;
+
+            // 이미지를 표시하는 로직
+            createMouseBox(mouseX, mouseY, images[Math.floor(Math.random() * images.length)]);
+
+            setCanShowImage(false);
+            setTimeout(() => {
+                setCanShowImage(true);
+            }, 500);
+        }
+    };
+
+    useEffect(() => {
+        const hoverWrapper = document.querySelector('.img_hover_play_wrap');
+
+        if (hoverWrapper) {
+            hoverWrapper.addEventListener('mousemove', handleMouseMove);
+
+            return () => {
+                hoverWrapper.removeEventListener('mousemove', handleMouseMove);
+            };
+        }
+    }, [handleMouseMove]);
+
+    const createMouseBox = (x, y, imageUrl) => {
+        console.log('Creating mouse box with image URL:', imageUrl);
+        const mouseBox = document.createElement('div');
+        mouseBox.className = 'mouseBox';
+    
+        const randomRotation = Math.floor(Math.random() * 30) - 10;
+    
+        mouseBox.style.left = x + 'px';
+        mouseBox.style.top = y + 'px';
+        mouseBox.style.transform = `rotate(${randomRotation}deg)`;
+    
+        const imgElement = document.createElement('img');
+        imgElement.src = imageUrl;
+        imgElement.className = 'lazyloaded';
+    
+        mouseBox.appendChild(imgElement);
+    
+        document.body.appendChild(mouseBox); // 여기서 실제 DOM에 추가
+    
+        setTimeout(() => {
+            mouseBox.style.opacity = 0;
+            setTimeout(() => {
+                mouseBox.remove();
+            }, 200);
+        }, 300);
+    };
+    
+    return (
         <>
             <div className="img_hover_play_wrap">
-                <div className="img_hover_play_bx">
-
+                <div className='character14'>
+                    <img src="./images/main/character/character14.png" alt="카세트" />
+                </div>
+                <div className='star'>
+                    <img src="./images/main/character/character9.gif" alt="" />
                 </div>
             </div>
         </>
