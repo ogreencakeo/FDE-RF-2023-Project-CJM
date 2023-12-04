cacec2e68db4fa373bde8f627c3b6e87555f2187
ogreencakeo <magente2000@gmail.com>
2023-12-04 12:11:59 +0900

    sdf

--- universalStudio/cjm-universal-project/src/contents/SchPage.jsx
+++ universalStudio/cjm-universal-project/src/contents/SchPage.jsx
@@ -1,7 +1,16 @@
+import { useLocation } from "react-router-dom";
+import { Seaching } from "./module/Seaching";
+
 export function SchPage(){
+    const loc = useLocation();
+    let keyword;
+
+    if(loc.state) keyword = loc.state.keyword;
+
     return(
         <>
             <h1 className="tit">검색 결과</h1>
+            <Seaching kword={keyword} />
         </>
     );
 }
