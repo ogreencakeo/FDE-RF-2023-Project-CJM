7706a2855bdb9d4b69b614598cfb587788941fdd
ogreencakeo <magente2000@gmail.com>
2023-12-04 18:17:32 +0900

    sdf

---
+++ universalStudio/cjm-universal-project/src/contents/pages/plugin/AreaSwiper.jsx
@@ -0,0 +1,62 @@
+import React from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+
+// Import Swiper styles
+import "swiper/css";
+import "swiper/css/navigation";
+import "swiper/css/pagination";
+
+import "../../../css/area_swiper.css";
+
+import { Navigation, Pagination, Autoplay} from "swiper/modules";
+// 데이터
+import { area_data } from "../../../data/module_data/area_data.js";
+import { Link } from "react-router-dom";
+
+export function AreaSwiper(props) {
+    const selData = area_data;
+
+    return (
+        <Swiper
+            slidesPerView={4}
+            spaceBetween={20}
+
+            pagination={{
+                clickable: true,
+            }}
+            loop={true}
+            navigation={true}
+            modules={[Pagination, Navigation, Autoplay]}
+            breakpoints={{
+                300: {
+                    slidesPerView: 1,
+                },
+                650: {
+                    slidesPerView: 2,
+                },
+                980 : {
+                    slidesPerView: 3,
+                },
+                1200: {
+                    slidesPerView: 4,
+                },
+            }}
+            className="mySwiper3"
+            autoplay={{
+                delay: 2000,
+                disableOnInteraction: false,
+            }}
+        >
+            {selData.map((v, i) => (
+                <SwiperSlide key={i}>
+                    <div className="area-swiper-bx">
+                        <div>
+                            <img src={`../images/${v.img}`} alt="에어리어 이미지" />
+                        </div>
+                        <h1>{v.name}</h1>
+                    </div>
+                </SwiperSlide>
+            ))}
+        </Swiper>
+    );
+} // SwiperApp 컴포넌트 //////////
