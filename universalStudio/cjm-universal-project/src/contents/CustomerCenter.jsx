c8346c5faffaabf93e839b5fb813aa7a8ee90565
ogreencakeo <magente2000@gmail.com>
2023-12-05 13:20:14 +0900

    sdf

--- universalStudio/cjm-universal-project/src/contents/CustomerCenter.jsx
+++ universalStudio/cjm-universal-project/src/contents/CustomerCenter.jsx
@@ -8,9 +8,8 @@ import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
 import { faCaretRight } from "@fortawesome/free-solid-svg-icons";
 
 export function CustomerCenter() {
-
-    const [customBtn, setCustomBtn] = useState('notice_announcement');
-    const [selData, setSelData] = useState(announcement[customBtn])
+    const [customBtn, setCustomBtn] = useState("notice_announcement");
+    const [selData, setSelData] = useState(announcement[customBtn]);
 
     const [customerColor, setCustomerColor] = useState("notice_announcement");
     const customerCenterFn = (data) => {
@@ -18,10 +17,16 @@ export function CustomerCenter() {
         setCustomerColor(data);
     };
 
-    useEffect(()=>{
+    useEffect(() => {
         const selectedData = announcement[customBtn];
         setSelData(selectedData);
     }, [customBtn]);
+
+    const cusmenu = document.querySelector('.cust-smenu-cont');
+
+    const viewCusCont = () => {
+        cusmenu.classList.toggle('on');
+    }
 
     return (
         <>
@@ -32,7 +37,8 @@ export function CustomerCenter() {
                     <div className="customer-option-bx">
                         <button
                             className={customerColor === "notice_announcement" ? "customerColorOn" : ""}
-                            onClick={() => customerCenterFn("notice_announcement")} key={0}
+                            onClick={() => customerCenterFn("notice_announcement")}
+                            key={0}
                         >
                             <span>공지사항</span>
                             {/* <span><FontAwesomeIcon icon={faCircleRight} /></span> */}
@@ -42,7 +48,8 @@ export function CustomerCenter() {
                         </button>
                         <button
                             className={customerColor === "frequently_asked" ? "customerColorOn" : ""}
-                            onClick={() => customerCenterFn("frequently_asked")} key={1}
+                            onClick={() => customerCenterFn("frequently_asked")}
+                            key={1}
                         >
                             <span>자주하는 질문</span>
                             <span>
@@ -51,7 +58,8 @@ export function CustomerCenter() {
                         </button>
                         <button
                             className={customerColor === "one-to-one-inquiry" ? "customerColorOn" : ""}
-                            onClick={() => customerCenterFn("one-to-one-inquiry")} key={2}
+                            onClick={() => customerCenterFn("one-to-one-inquiry")}
+                            key={2}
                         >
                             <span>1대1 문의</span>
                             <span>
@@ -70,42 +78,73 @@ export function CustomerCenter() {
                             <div>작성자</div>
                             <div>작성일</div>
                         </div>
-                        {selData.map((v, i) => (
-                            <ul className="customerCenter-smenu" key={i}>
-                                <li>{v.idx}</li>
-                                <li>{v.tit}</li>
-                                <li>{v.writer}</li>
-                                <li>{v.date}</li>
-                                <div className="Cust-smenu-cont"></div>
-                            </ul>
-                        ))}
+                        {/* {selData['notice_announcement'] && (
+                            <>
+                                <div>
+                                    <div>번호</div>
+                                    <div>제목</div>
+                                    <div>작성자</div>
+                                    <div>작성일</div>
+                                </div>
+                                {selData.map(
+                                    (v, i) =>
+                                        v.writer != "" && (
+                                            <ul className="customerCenter-smenu" key={i}>
+                                                <li>{v.idx}</li>
+                                                <li>{v.tit}</li>
+                                                <li>{v.writer}</li>
+                                                <li>{v.date}</li>
+                                                <div className="Cust-smenu-cont"></div>
+                                            </ul>
+                                        )
+                                )}
+                            </>
+                        )}
+                        {selData == "frequently_asked" && (
+                            <>
+                                <div>
+                                    <div>번호</div>
+                                    <div>제목</div>
+                                </div>
+                                {selData.map(
+                                    (v, i) =>
+                                        v.writer == "" && (
+                                            <ul className="customerCenter-smenu" key={i}>
+                                                <li>{v.idx}</li>
+                                                <li>{v.tit}</li>
+                                                <div className="Cust-smenu-cont"></div>
+                                            </ul>
+                                        )
+                                )}
+                            </>
+                        )} */}
+                        {selData.map(
+                            (v, i) =>
+                                v.writer != "" && (
+                                    <>
+                                        <ul className="customerCenter-smenu" key={i} onClick={()=>viewCusCont()}>
+                                            <li>{v.idx}</li>
+                                            <li>{v.tit}</li>
+                                            <li>{v.writer}</li>
+                                            <li>{v.date}</li>
+                                        </ul>
+                                        <div className="cust-smenu-cont">
+                                            {v.cont}
+                                        </div>
+                                    </>
+                                )
+                        )}
+                        {selData.map(
+                            (v, i) =>
+                                v.writer == "" && (
+                                    <ul className="customerCenter-smenu" key={i}>
+                                        <li>{v.idx}</li>
+                                        <li>{v.tit}</li>
+                                        <div className="Cust-smenu-cont"></div>
+                                    </ul>
+                                )
+                        )}
                     </div>
-                    {/* <table>
-                        <tr>
-                            <th>번호</th>
-                            <th>제목</th>
-                            <th>작성자</th>
-                            <th>작성일</th>
-                        </tr>
-                        {
-                            announcement.map((v, i) => 
-                                <tr key={i}>
-                                    <td>{v.idx}</td>
-                                    <td>{v.tit}</td>
-                                    <td>{v.writer}</td>
-                                    <td>{v.date}</td>
-                                </tr>
-                            )
-                        }
-                    </table> */}
-                    {/* <div className="customerCenter-bx">
-                        <div className="customerCenter-cont">
-                            <h2>번호</h2>
-                            <h2>제목</h2>
-                            <h2>작성자</h2>
-                            <h2>작성일</h2>
-                        </div>
-                    </div> */}
                 </div>
             </div>
         </>
