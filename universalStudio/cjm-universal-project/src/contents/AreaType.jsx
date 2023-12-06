aa3f689a8664fcf3d70dbd1f3eed75483af30229
ogreencakeo <magente2000@gmail.com>
2023-12-06 13:16:35 +0900

    sdf

--- universalStudio/cjm-universal-project/src/contents/AreaType.jsx
+++ universalStudio/cjm-universal-project/src/contents/AreaType.jsx
@@ -1,10 +1,51 @@
-import { Route, Routes } from "react-router-dom";
-import { Nintendo } from "./areaType/Nintendo";
+
+import { useEffect, useState } from "react";
+import { areaData } from "../data/attraction/attractionData"
+
+import '../css.area_type.css';
 
 export function AreaType(){
+
+    const [areaType, setAreaType] = useState('nintendo');
+    const [selData, setSelData] = useState(areaData[areaType]);
+
+    const areaChangeFn = (data) => {
+        setAreaType(data);
+    }
+
+    useEffect(()=>{
+        const selectedData = areaData[areaType];
+        setSelData(selectedData);
+    }, [areaType]);
+
     return(
         <>
-            
+            <div className="area-type-btn">
+                <nav>
+                    <ul>
+                        <li><button onClick={() => areaChangeFn('nintendo')} key={0}>슈퍼 닌텐도 월드™</button></li>
+                        <li><button onClick={() => areaChangeFn('harrypotter')}  key={1}>위저딩 월드 오브 해리 포터™</button></li>
+                        <li><button onClick={() => areaChangeFn('minion')}  key={2}>미니언 파크</button></li>
+                        <li><button onClick={() => areaChangeFn('wonderland')}  key={3}>원더랜드</button></li>
+                        <li><button onClick={() => areaChangeFn('jurassicpark')}  key={4}>쥬라기 공원</button></li>
+                    </ul>
+                </nav>
+            </div>
+            <div className="area-type-wrap">
+                {
+                    selData.map((v, i) =>
+                        <div className="area-type-bx" key={i}>
+                            <div className="area-type-img">
+                                <img src={v.img} alt="" />
+                            </div>
+                            <div className="area-type-cont">
+                                <h3>{v.areatype}</h3>
+                                <h2>{v.name}</h2>
+                            </div>
+                        </div>
+                    )
+                }
+            </div>
         </>
     )
 }
