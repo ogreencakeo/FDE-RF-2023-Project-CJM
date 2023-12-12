bbeed6b664da1ac8d8aa4217f428e818425e995b
jimin0427 <cjm0427@hs.ac.kr>
2023-12-12 23:45:03 +0900

    fds

--- bio/cjm-bio-project/src/contents/layout/FooterArea.jsx
+++ bio/cjm-bio-project/src/contents/layout/FooterArea.jsx
@@ -1,5 +1,12 @@
 export function FooterArea(){
     return(
-        <></>
+        <div className="footer-wrap">
+            <div className="footer">
+                <h1>SAMSUNG BIOLOGICS</h1>
+                <h2>SAMSUNG</h2>
+                <p>인천광역시 연수구 송도바이오대로 300</p>
+                <h2>Copyright ⓒ 2023 Samsung Biologics. All rights reserved.</h2>
+            </div>
+        </div>
     );
 }
