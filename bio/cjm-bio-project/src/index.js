3682fcdcbe376b5ac9dc2fe695a7138784626ce6
jimin0427 <cjm0427@hs.ac.kr>
2023-12-12 20:18:41 +0900

    asd

--- bio/cjm-bio-project/src/index.js
+++ bio/cjm-bio-project/src/index.js
@@ -2,13 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 
+// 라우터
+import {BrowserRouter, Route, Routes} from "react-router-dom"
 
-export function App() {
+// 모듈 불러오기
+import { Layout } from '../contents/layout/Layout';
+import {Main} from '../contents/pages/Main'
+
+export default function App() {
   return(
     <BrowserRouter>
       <Routes>
-        <Route>
-          
+        <Route path='/' element={<Layout />}>
+          <Route index element={<Main />} />
         </Route>
       </Routes>
     </BrowserRouter>
