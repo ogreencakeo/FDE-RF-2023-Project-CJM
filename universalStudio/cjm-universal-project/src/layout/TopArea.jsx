8a8062bbdf6edb6847e2de28ee79511b24af1ab1
jimin0427 <cjm0427@hs.ac.kr>
2023-12-14 20:46:49 +0900

    asd

--- universalStudio/cjm-universal-project/src/layout/TopArea.jsx
+++ universalStudio/cjm-universal-project/src/layout/TopArea.jsx
@@ -38,28 +38,33 @@ export const TopArea = memo(({ chgPageFn, logSts, logMsg, logOut }) => {
     };
 
     const showMenu = () => {
-        $('.top-area').toggleClass('on');
+        // $('.top-area').toggleClass('on');
+        $('.top-show-nav').toggleClass('on');
     }
 
 
-    useEffect(()=>{
+    useEffect(() => {
         // GNB a 요소 클릭시 전체메뉴 닫기
         // 대상 : .gnb a[href!='#']
         // -> href가 '#'이 아닌 gnb 하위 모든 a요소
         // -> !=은 제이쿼리 전용!
-        $(".gnb a[href!='#']").on('click', ()=>{
-            $('.top-area').removeClass('on')
+        $(".gnb2 a[href!='#']").on('click', () => {
+            $('.top-show-nav').removeClass('on');
         }); // click /////////
 
-        
+
         // let hv = smenu.offsetHeight;
         // smenu.style.height = hv + 'px';
-        
+
     }); // useEffect
 
-    useEffect(()=>{
-        let smenu = document.querySelectorAll('.smenu');
-        let hv = smenu.querySelector('ol').innerHeight;
+    useEffect(() => {
+        // let smenu = document.querySelectorAll('.smenu');
+        // let hv = document.querySelector('.smenu ol').cleintHeight;
+        // document.addEventListener('.gnb li', 'onmousemove', ()=>{
+        //     smenu.style.height = hv + 'px';
+        // })
+
     })
 
 
@@ -123,6 +128,57 @@ export const TopArea = memo(({ chgPageFn, logSts, logMsg, logOut }) => {
                     <button className="hambtn" onClick={showMenu}></button>
                 </nav>
             </header>
+            <div className="top-show-nav">
+                <nav className="gnb2">
+                    <ul>
+
+                        {navMenu.map((v, i) => (
+                            <li key={i}>
+                                {v.sub ? <a href="#">{v.txt}</a> : <Link to={v.link}>{v.txt}</Link>}
+                                {v.sub && (
+                                    <div className="smenu2">
+                                        <ol>
+                                            {v.sub.map((v, i) => (
+                                                <li key={i}>
+                                                    <Link to={v.link}>{v.txt}</Link>
+                                                </li>
+                                            ))}
+                                        </ol>
+                                    </div>
+                                )}
+                            </li>
+                        ))}
+                        <li style={{ marginLeft: "auto" }}>
+                            <div className="searchingGnb">
+                                <FontAwesomeIcon icon={faSearch} className="schbtnGnb" title="Open Search" />
+                                <input id="schinGnb" type="text" placeholder="Filter by keyword" onKeyUp={enterKey} />
+                            </div>
+                            <a href="#" onClick={showSerach}>
+                                <FontAwesomeIcon icon={faSearch} />
+                            </a>
+                        </li>
+                        {logSts === null && (
+                            <>
+                                <li>
+                                    <Link to="/member">회원가입</Link>
+                                </li>
+                                <li>
+                                    <Link to="/login">로그인</Link>
+                                </li>
+                            </>
+                        )}
+                        {
+                            logSts !== null && (
+                                <>
+                                    <li>
+                                        <a href="#" onClick={logOut}>LOGOUT</a>
+                                    </li>
+                                </>
+                            )
+                        }
+                    </ul>
+                </nav>
+            </div>
         </>
     );
 });
