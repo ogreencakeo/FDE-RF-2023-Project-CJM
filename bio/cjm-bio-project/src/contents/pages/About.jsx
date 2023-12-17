06a6f7401b82a3ebd17ec4d3d493f2f3c3abf41a
jimin0427 <cjm0427@hs.ac.kr>
2023-12-17 23:31:56 +0900

    asd

--- bio/cjm-bio-project/src/contents/pages/About.jsx
+++ bio/cjm-bio-project/src/contents/pages/About.jsx
@@ -1,7 +1,23 @@
-export function About(){
-    return(
+import '../../css/about.css';
+
+export function About() {
+    return (
         <>
-            About 페이지
+            <div className="about-page-wrap">
+                <h1>Driven For Your Life</h1>
+                <div className="person-img">
+                    <img src="../images/about/person2.png" alt="사람" />
+                </div>
+                <p>Driven. For Life.
+                    ‘생명을 향한 의지’는 삼성바이오로직스의 DNA에 내재된
+                    정체성이자 더 나은 삶을 향한 우리의 약속입니다.
+                    더 건강한 삶을 위해 멈추지 않는 도전은
+                    삼성바이오로직스만의 DNA에서 비롯됩니다.
+                </p>
+            </div>
+            <div className="about-us">
+                <h1>About Us</h1>
+            </div>
         </>
     );
 }
