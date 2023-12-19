805b89a757a974016602a7379554badccd003c33
ogreencakeo <magente2000@gmail.com>
2023-12-19 16:33:30 +0900

    Update TopArea.jsx

--- universalStudio/cjm-universal-project/src/layout/TopArea.jsx
+++ universalStudio/cjm-universal-project/src/layout/TopArea.jsx
@@ -19,7 +19,7 @@ export const TopArea = memo(({ chgPageFn, logSts, logMsg, logOut }) => {
     // enterKey
     const enterKey = (e) => {
         $(e.target).bind( 'keydown',function (e) {
-            if (e.key === "Enter") {
+            if (e.key === "Enter" || e.keyCode === 13) {
                 // console.log('e.target :', e.target);
                 let txt = $(e.target).val().trim();
                 console.log(txt);
