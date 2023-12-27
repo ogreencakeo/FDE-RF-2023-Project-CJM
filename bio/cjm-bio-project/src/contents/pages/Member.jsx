b8ab9950ef60df1b1780d56082b59387854184f4
ogreencakeo <magente2000@gmail.com>
2023-12-27 17:22:07 +0900

    sdf

--- bio/cjm-bio-project/src/contents/pages/Member.jsx
+++ bio/cjm-bio-project/src/contents/pages/Member.jsx
@@ -1,7 +1,37 @@
+// 로컬스트로지 생성 JS
+import { useContext, useState } from 'react';
+import {initData} from '../data/mem_fn.js';
+
+// 컨텍스트 API 불러오기
+import { bioConn } from '../modules/bioContent.jsx';
+
 export function Member(){
+    const myCon = useContext(bioConn);
+
+    // 입력요소 상태변수
+    const [userId, setUserId] = useState('');
+    const [pwd, setPwd] = useState('');
+    const [chkPwd, setChkPwd] = useState('');
+    const [userName, setUserName] = useState('');
+    const [email, setEmail] = useState('');
+
+    // 에러 상태관리 변수
+    const [ userIdError, setUserIdError] = useState(false);
+    
+
     return(
-        <>
-            Member 페이지
-        </>
+        <div className="outbx">
+            <section className="membx">
+                <h2>회원가입</h2>
+                <form action="process.php" method='post'>
+                    <ul>
+                        <li>
+                            <label>ID : </label>
+                            <input type="text" maxLength="20" placeholder='Please enter your ID' />
+                        </li>
+                    </ul>
+                </form>
+            </section>
+        </div>
     )
 }
