7f259422a75b17f537e92e0e646f1e347d1b5556
jimin0427 <cjm0427@hs.ac.kr>
2023-12-29 23:47:33 +0900

    sdfsf

--- etude/data/indexData.js
+++ etude/data/indexData.js
@@ -1,3 +1,90 @@
 const navData = [
     "PRODUCT", "BRAND", "MAKEUP LOOK", "STORE", "FOUNDATION FACTORY"
 ];
+
+const mainBanImg = [
+    '../image/swiperBanner/main/1.jpg', 
+    '../image/swiperBanner/main/2.jpg', 
+    '../image/swiperBanner/main/3.jpg', 
+    '../image/swiperBanner/main/4.jpg', 
+];
+
+const main1Data = [
+    {
+        img : '../image/main/main1/1.png',
+        txt : '팍싱틴트',
+        cont : '12,000원'
+    },
+    {
+        img : '../image/main/main1/2.png',
+        txt : '플레이 컬러 아이즈',
+        cont : '30,000원'
+    },
+    {
+        img : '../image/main/main1/3.png',
+        txt : '컬 픽스 마스카라',
+        cont : '15,000원'
+    },
+    {
+        img : '../image/main/main1/4.png',
+        txt : '그림자 쉐딩',
+        cont : '20,000원'
+    },
+    {
+        img : '../image/main/main1/5.png',
+        txt : '순정 시카 카밍 앰플 에센스',
+        cont : '29,000원'
+    },
+];
+
+const main3Data = [
+    "../image/main/main3/1.jpg",
+    "../image/main/main3/2.jpg",
+];
+
+const main4Data = [
+    
+    {
+        img : "../image/main/main4/1.jpg",
+        txt : "순정 2x 베리어 보습 크림 60ml",
+        cont : "#보습크림 #순정",
+        price : "17,000원"
+    },
+    {
+        img : "../image/main/main4/2.jpg",
+        txt : "순정 시카 카밍 앰플 에센스 50ml",
+        cont : "#순정 #앰플에센스",
+        price : "29,000원"
+    },
+    {
+        img : "../image/main/main4/3.jpg",
+        txt : "순정 약산성 5.5 폼 클렌저 150ml",
+        cont : "#순정 #약산성 #폼클렌징",
+        price : "12,000원"
+    },
+    {
+        img : "../image/main/main4/4.jpg",
+        txt : "순정 수분 베리어 크림 75ml",
+        cont : "#수분크림 #순정",
+        price : "22,000원"
+    },
+    {
+        img : "../image/main/main4/5.jpg",
+        txt : "순정 약산성 6.5 휩 클렌저 150ml",
+        cont : "#순정 #약산성",
+        price : "11,000원"
+    },
+    {
+        img : "../image/main/main4/6.jpg",
+        txt : "순정 약산성 5.5 진정 토너",
+        cont : "#순정 #약산성",
+        price : "12,000원"
+    },
+];
+
+const giftBxData = [
+    "../image/main/gift/1.webp",
+    "../image/main/gift/2.jpg",
+    "../image/main/gift/3.jpg",
+    "../image/main/gift/4.jpg",
+]
--- etude/js/index.js
+++ etude/js/index.js
@@ -1,9 +1,107 @@
 
 const navList = document.querySelector('.nav-wrap ul');
-for(let i=0; i<navData.length; i++){
-    const temp = [];
+let temp = [];
+for (let i = 0; i < navData.length; i++) {
     temp[i] = `
         <li>${navData[i]}</li>
     `
 }
-navList.innerHTML += temp;
+navList.innerHTML += temp.join('');
+
+
+// head Swpier
+const swiperContainer = new Swiper('.header-swiper-container', {
+    slidesPerView: 1,
+    spaceBetween: 10,
+    pagination: {
+        el: "swiper-pagination",
+        clickable: true,
+        renderBullet: function (index, className) {
+            return '<span class="' + className + '">' + (index + 1) + "</span>";
+        },
+    },
+});
+
+for (let i = 0; i < mainBanImg.length; i++) {
+    const swiperSlide = document.createElement('div');
+    swiperSlide.classList.add('swiper-slide');
+
+    const imgElement = document.createElement('img');
+    imgElement.src = mainBanImg[i];
+
+    swiperSlide.appendChild(imgElement);
+    swiperContainer.appendSlide(swiperSlide);
+}
+
+swiperContainer.update(); // Swiper 업데이트
+
+
+
+// main1
+const main1Bx = document.querySelector('.main1-wrap');
+
+temp = [];
+main1Data.map((v, i) =>{
+    temp[i] =`
+        <div class='main1Bx'>
+            <div class='main1Img'>
+                <img src=${v.img} alt='메인이미지' />
+            </div>
+            <section>
+                <p>${v.txt}</p>
+                <h2>${v.cont}</h2>
+            </section>
+        </div>
+    `
+});
+
+main1Bx.innerHTML += temp;
+
+// main3
+const main3Bx = document.querySelector('.main3-wrap');
+temp = [];
+
+for(let i=0; i<main3Data.length; i++){
+    temp[i] = `
+        <div class='main3Bx'>
+            <div class='main3-img'>
+                <img src=${main3Data[i]} alt='main3사진'/>
+                <span></span>
+            </div>
+        </div>
+    `
+}
+
+main3Bx.innerHTML += temp.join('');
+
+const main4Bx = document.querySelector('.main4-img-l');
+
+temp = [];
+main4Data.map((v, i) => {
+    temp[i] = `
+        <div class='main4Bx'>
+            <div class='main4Bx-img'>
+                <img src=${v.img} alt='main4 이미지' />
+            </div>
+            <section>
+                <h2>${v.cont}</h2>
+                <h1>${v.txt}</h1>
+                <h2>${v.price}</h3>
+            </section>
+        </div>
+    `
+});
+
+main4Bx.innerHTML += temp.join('');
+
+const giftBx = document.querySelector('.gift-bx-wrap');
+temp = [];
+for(let i=0; i<giftBxData.length; i++){
+    temp[i] = `
+        <div class='gift-bx'>
+            <img src=${giftBxData[i]} alt='선물이미지' />
+        </div>
+    `
+}
+
+giftBx.innerHTML += temp.join('');
