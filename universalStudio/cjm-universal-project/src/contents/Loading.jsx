fe7fcfbc6acd9da6ffec668d7b8a5267273ee999
jimin0427 <cjm0427@hs.ac.kr>
2024-01-03 23:34:46 +0900

    sadsda

--- universalStudio/cjm-universal-project/src/contents/Loading.jsx
+++ universalStudio/cjm-universal-project/src/contents/Loading.jsx
@@ -1,9 +1,34 @@
-
 import { createElement, useEffect, useState } from 'react';
 import '../css/loading.css';
 
 export function Loading() {
-    const loadingTxt = 'UNIVERSALSTUDIO';
+    const loadingTxt = 'LOADING';
+    // const loadImg = [
+    //     '/images/loading/1.png',
+    //     '/images/loading/2.png',
+    //     '/images/loading/3.png',
+    //     '/images/loading/4.png',
+    // ];
+
+    const [progress, setProgress] = useState(0);
+
+    
+    useEffect(() => {
+        const progressBarInterval = setInterval(() => {
+            setProgress((prevProgress) => {
+                const newProgress = (prevProgress + 1) % 101;
+
+                // 로딩이 완료되면 interval 정리
+                if (newProgress === 100) {
+                    clearInterval(progressBarInterval);
+                }
+
+                return newProgress;
+            });
+        }, 3000 / 100); 
+
+        return () => clearInterval(progressBarInterval);
+    }, []);
 
     const makeCode = () => {
         return loadingTxt.split('').map((v, i) => (
@@ -13,33 +38,20 @@ export function Loading() {
         ));
     };
 
-    const loadImg = [
-        '/images/loading/1.png',
-        '/images/loading/2.png',
-        '/images/loading/3.png',
-        '/images/loading/4.png',
-    ];
-
-    const [currentImgIndex, setCurrentImgIndex] = useState(0);
-
-    useEffect(() => {
-        const intervalId = setInterval(() => {
-            setCurrentImgIndex((i) => (i + 1) % loadImg.length);
-        }, 1100);
-
-        return () => clearInterval(intervalId);
-    }, [currentImgIndex, loadImg]);
-
     return (
         <div className="loadpage-page">
             <div>
                 <div className='loadpage-img-bx'>
                     {/* 이미지를 상태에 따라 렌더링 */}
-                    <img src={process.env.PUBLIC_URL + loadImg[currentImgIndex]} alt="Loading" />
+                    <img src={process.env.PUBLIC_URL + '/images/loading/1.png'} alt="Loading" />
                 </div>
                 <h1 className="loading-text">
                     {makeCode()}
                 </h1>
+                {/* 프로그레스 바 */}
+                <div className="progress-bar">
+                    <div className="progress" style={{ width: `${progress}%` }}></div>
+                </div>
             </div>
         </div>
     );
