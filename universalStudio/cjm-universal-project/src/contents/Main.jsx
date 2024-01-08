7d3a8e48649f6f7dcbe9f3214407eba505160749
jimin0427 <cjm0427@hs.ac.kr>
2024-01-09 03:15:51 +0900

    Update Main.jsx

--- universalStudio/cjm-universal-project/src/contents/Main.jsx
+++ universalStudio/cjm-universal-project/src/contents/Main.jsx
@@ -322,7 +322,7 @@ export function Main() {
                         </div>
                         <div className="modal-btn-bx">
                             <button className="closeBtn">X</button>
-                            <button>하루동안 닫기</button>
+                            <button>3분동안 닫기</button>
                         </div>
                     </div>
                 </div>
