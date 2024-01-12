f8a753299f51e4e572bddda8e6165dffb3e573d2
ogreencakeo <magente2000@gmail.com>
2024-01-12 18:18:07 +0900

    sdf

--- etude/js/sub.js
+++ etude/js/sub.js
@@ -2,6 +2,31 @@ window.onload = function () {
     window.scrollTo(0, 0);
 };
 
+// 스크롤시 Top 버튼 생기게 하기 
+const scrollBtn = document.querySelector('.scroll-btn');
+console.log('scrollBtn :', scrollBtn);
+
+const topBtnShow = () => {
+    const scrollPostion = window.scrollY || window.pageYOffset;
+    if(scrollBtn){
+        if(scrollPostion > 2000){
+            scrollBtn.style.display = 'block';
+        }else{
+            scrollBtn.style.display = 'none';
+        }
+    }
+};
+
+window.addEventListener('scroll', topBtnShow);
+
+
+// Top 버튼 클릭시 위로 올라가게 하기
+const topBtnScroll = () => {
+    window.scrollTo(0, 0);
+};
+
+scrollBtn.addEventListener('click', topBtnScroll);
+
 // nav
 // const navList = document.querySelector(".nav-wrap ul");
 // let temp = [];
