d5030b2b9c53f319bf950b830a07460b95ab7b44
jimin0427 <cjm0427@hs.ac.kr>
2024-01-27 22:07:37 +0900

    asdsad

--- etude/data/factoryData.js
+++ etude/data/factoryData.js
@@ -20,3 +20,89 @@ const navData = [
         link : './foundation_factory.html'
     },
 ];
+
+const customized = [
+    {
+        icon : 'fa-regular fa-clock',
+        title : '소요 시간',
+        txt : '50분 · 30분',
+    },
+    {
+        icon : 'fa-solid fa-store',
+        title : '체험 장소',
+        txt : '에뛰드 명동1번가점',
+    },
+    {
+        icon : 'fa-solid fa-calendar-days',
+        title : '예약 일시',
+        txt : '매월 26일 00시 예약 오픈',
+        subtxt : '*다음 달 1일 ~ 31일'
+    },
+    {
+        icon : 'fa-solid fa-user-doctor',
+        title : '체험 내용',
+        txt : '피부톤 진단 · 드레이핑 컬러 진단 · 100가지 컬러 · 로봇 즉석 제조',
+    },
+];  
+
+const makeOwnData = [
+    {
+        img : '../image/foundation_factory/customized/1.jpg',
+        txt : 'CHECK^YOUR^SKINTONE',
+    },
+    {
+        img : '../image/foundation_factory/customized/2.jpg',
+        txt : 'PERSONAL^COLOR^TEST',
+    },
+    {
+        img : '../image/foundation_factory/customized/3.jpg',
+        txt : 'PICK^THE^COLOR',
+    },
+    {
+        img : '../image/foundation_factory/customized/4.jpg',
+        txt : 'MADE^by^ROBOT',
+    },
+    {
+        img : '../image/foundation_factory/customized/5.jpg',
+        txt : 'RECOMMEND^FOR^YOU',
+    },
+    {
+        img : '../image/foundation_factory/customized/6.jpg',
+        txt : 'MEET^YOUR^FOUNDATION',
+    },
+];
+
+const factoryNoticeData = [ 
+    '온라인 예약 후 서비스를 이용하실 수 있습니다. 매장 방문 시 예약 정보를 보여 주세요.',
+    '예약 취소는 예약을 진행한 사이트에서만 가능합니다.',
+    '예약 시간에서 10분 이상 늦을 경우 다음 타임 고객님의 서비스를 위하여 예약은 자동 취소됩니다.',
+    '앞 타임 고객님의 응대로 서비스 시작 시간이 약 10분 정도는 지연될 수 있음을 양해 부탁드립니다.',
+    '더욱 정확한 퍼스널 컬러 측정을 원하신다면 컬러 메이크업은 최소한으로 하고 오시는 것이 좋습니다.'
+];
+
+const storePositionData = [
+    {
+        icon : 'fa-regular fa-clock',
+        title : '오픈',
+        txt : '매장 10:00 ~ 23:00',
+        subtxt : '파운데이션 팩토리 11:00 ~ 20:00',
+    },
+    {
+        icon : "fa-solid fa-phone",
+        title : '오픈',
+        txt : '010-6679-8855 (서비스중 부재 시, 네이버 톡톡 문의)',
+    },
+    {
+        icon : "fa-solid fa-location-dot",
+        title : '주소',
+        txt : '서울 중구 명동4길 11',
+        subtxt : '서울 중구 명동2가 54-1',
+    },
+];
+
+const takeTourData = [
+    '../image/foundation_factory/1.jpg',
+    '../image/foundation_factory/2.jpg',
+    '../image/foundation_factory/3.jpg',
+    '../image/foundation_factory/4.jpg',
+]
--- etude/js/foundation_factory.js
+++ etude/js/foundation_factory.js
@@ -61,3 +61,116 @@ const topBtnScroll = () => {
 };
 
 scrollBtn.addEventListener('click', topBtnScroll);
+
+
+// 맞춤서비스
+const customizedService = document.querySelector('.customized-service-bx-r');
+temp = [];
+
+customized.map((v, i) => {
+    temp[i] = `
+        <div class='customized-service-cont'>
+            <p class="${v.icon}"></p>
+            <p>${v.title}</p>
+            <p>${v.txt}</p>
+            <p>${v.subtxt? v.subtxt : ''}</p>
+        </div>
+    `
+}); 
+
+customizedService.innerHTML += temp.join('');
+
+// 모델 배너 문구 setTimeOut으로 설정함
+const facotryBannerCont = document.querySelector('.factory-banner-cont p span:last-child');
+const bannerTxt = [
+    '21C1',
+    '21.5N1',
+    '13.5C2', 
+    '17.5W1'
+];
+let index = 0;
+
+setInterval(()=>{
+    index = index + 1;
+    if(index >= bannerTxt.length) index = 0;
+    facotryBannerCont.innerHTML = bannerTxt[index];
+},300);
+
+// 나만의 파운데이션 만들기
+const makeOwnWrap = document.querySelector('.make-own-bx-wrap');
+temp = [];
+makeOwnData.map((v, i) => {
+    temp[i] = `
+        <div class='make-own-bx'>
+            <img src = "${v.img}" alt="${v.txt}" />
+            <div class='make-own-cont'>
+                <p>${v.txt.split('^')[0]}</p>
+                <p>${v.txt.split('^')[1]}</p>
+                <p>${v.txt.split('^')[2]}</p>
+            </div>
+        </div>
+    `
+});
+
+makeOwnWrap.innerHTML += temp.join('');
+
+// 팩토리 - 상점 위치
+// 맞춤서비스
+const storePosition = document.querySelector('.store-position-bx-r');
+temp = [];
+
+storePositionData.map((v, i) => {
+    temp[i] = `
+        <div class='store-position-cont'>
+            <p class="${v.icon}"></p>
+            <p>${v.title}</p>
+            <p>${v.txt}</p>
+            <p>${v.subtxt? v.subtxt : ''}</p>
+        </div>
+    `
+}); 
+
+storePosition.innerHTML += temp.join('');
+
+// 팩토리 둘러보기
+const takeTour = document.querySelector('.factory-take-tour');
+temp = [];
+for(let i=0; i<takeTourData.length; i++){
+    temp[i] = `
+        <div class='factory-take-tour-bx'>
+            <img src="${takeTourData[i]}" alt="파운데이션팩토리 이미지"/>
+        </div>
+    `
+}
+
+takeTour.innerHTML += temp.join('');
+
+// 팩토리 - 공지
+const storeNotice = document.querySelector('.store-notice-bx');
+temp = [];
+
+for(let i=0; i<factoryNoticeData.length; i++){
+    temp[i] =  `
+        <div class='store-notice-cont'>
+            <ul>
+                <li>${factoryNoticeData[i]}</li>
+            </ul>
+        </div>
+    `;
+}
+
+storeNotice.innerHTML += temp.join('');
+
+// 스크롤 내릴시 공지 애니메이션
+const storeNoticeBx = document.querySelector('.store-notice-bx');
+
+const showNotice = () => {
+    const storeNoticeBxPosition = storeNoticeBx.getBoundingClientRect().top;
+    if(storeNoticeBxPosition < window.innerHeight){
+        storeNoticeBx.classList.add('on');
+    }else{
+        storeNoticeBx.classList.remove('on');
+    }
+};
+
+window.addEventListener('scroll', showNotice);
