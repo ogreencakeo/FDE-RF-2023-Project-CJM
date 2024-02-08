9e8ebfa116c0701ce9ec22bb9ce491889b1b346f
jimin0427 <cjm0427@hs.ac.kr>
2024-02-08 19:27:43 +0900

    czsc

--- portfolio_pj/js/index.js
+++ portfolio_pj/js/index.js
@@ -6,7 +6,7 @@ let temp = [];
 introduceData.map((v, i) => {
     temp[i] = `
         <div class='introduce-bx'>
-            <h2><p class="fa-regular fa-circle-check"></p>${v.tit}</h2>
+            <h2><p class="fa-regular fa-circle-check"></p> ${v.tit}</h2>
             <p>${
                 v.tit === '공모전' ? (
                     `<a href='./crape.pdf' target='_blank'><i class="fa-solid fa-link"></i> ${v.txt}</a><br />
@@ -38,7 +38,7 @@ temp = [];
 project1UrlData.map((v, i) => {
     temp[i] = `
         <div class='project1-url-bx'>
-            <h2><p class="fa-regular fa-circle-check"></p>${v.tit}</h2>
+            <h2><p class="fa-regular fa-circle-check"></p> ${v.tit}</h2>
             <p>
                 <a href=${v.url} target='_blank'><i class="fa-solid fa-link"></i> ${v.url}</a>
             </p>
@@ -66,7 +66,7 @@ temp = [];
 project2UrlData.map((v, i) => {
     temp[i] = `
         <div class='project2-url-bx'>
-            <h2><p class="fa-regular fa-circle-check"></p>${v.tit}</h2>
+            <h2><p class="fa-regular fa-circle-check"></p> ${v.tit}</h2>
             <p>
                 <a href=${v.url} target='_blank'><i class="fa-solid fa-link"></i> ${v.url}</a>
             </p>
@@ -76,17 +76,70 @@ project2UrlData.map((v, i) => {
 
 project2Url.innerHTML += temp.join('');
 
-
-// 헤더 색상 변경
+// 스크롤시 색상 변경
 const headerColor = document.querySelectorAll('.header-nav li a');
 
+let stsClick = 0;
+let btnScrollY;
+
+let sectionPositions;
+
+const updateButtonPositions = () => {
+    sectionPositions = Array.from(headerColor).map((link) => {
+        const targetId = link.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+        return targetSection.offsetTop;
+    });
+};
+
+const scrollFn = () => {
+    if (stsClick) return;
+    btnScrollY = window.scrollY || document.documentElement.scrollTop;
+
+    for (let i = sectionPositions.length - 1; i >= 0; i--) {
+        if (btnScrollY >= sectionPositions[i]) {
+            setActiveClass(i);
+            break;
+        }
+    }
+};
+
+const setActiveClass = (index) => {
+    headerColor.forEach((v) => v.classList.remove('color-change'));
+    headerColor[index].classList.add('color-change');
+};
+
+window.addEventListener('scroll', scrollFn);
+
 headerColor.forEach((v, i) => {
-    v.addEventListener('click' , function(){
+    v.addEventListener('click', function (event) {
+        event.preventDefault();
         headerColor.forEach((j) => {
             j.classList.remove('color-change');
-        })
+        });
         v.classList.add('color-change');
+        stsClick = true;
+
+        const targetId = v.getAttribute('href').substring(1);
+        const targetSection = document.getElementById(targetId);
+
+        window.scrollTo({
+            top: targetSection.offsetTop,
+            behavior: 'smooth',
+        });
+
+        setTimeout(() => {
+            stsClick = false;
+        }, 1000); // 클릭 후 1초 동안은 스크롤 이벤트 무시
     });
 });
 
+// 초기 색상 설정
 headerColor[0].click();
+
+// 초기화 함수 호출
+updateButtonPositions();
+
+// 창 크기 변경에 대응하기 위한 리사이즈 이벤트 추가
+window.addEventListener('resize', updateButtonPositions);
+
