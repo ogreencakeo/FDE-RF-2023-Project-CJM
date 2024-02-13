b5930ae373962e43368cf00f3f5b1469f8cc6f97
jimin0427 <cjm0427@hs.ac.kr>
2024-02-14 00:58:16 +0900

    sad

--- pratice_01/my-first-react-app/src/page/Board.jsx
+++ pratice_01/my-first-react-app/src/page/Board.jsx
@@ -1,30 +1,28 @@
-import { Fragment, useRef, useState } from "react"
-import baseData from "../data/유니버설-문의2.json";
-import "../css/board.css";
-import $ from "jquery";
+import { Fragment, useRef, useState } from "react";
 
-baseData.sort((a,b) => {
-    return Number(a.idx)===Number(b.idx)? 0 : Number(a.idx) > Number(b.idx)? -1 : 1;
+import baseData from '../data/유니버설-문의2.json';
+import '../css/board.css';
+
+baseData.sort((a, b) => {
+    return Number(a.idx) === Number(b.idx) ? 0 : Number(a.idx) > Number (b.idx)? -1 : 1;
 });
 
 let orgData;
-
-if(localStorage.getItem('pratice-bdata')) orgData = localStorage.getItem('pratice-bdata');
+if(localStorage.getItem('pratice-bdata')) orgData = JSON.parse(localStorage.getItem('pratice-bdata'));
 else orgData = baseData;
 
 export function Board(){
+
     if(!localStorage.getItem('pratice-bdata')){
-        localStorage.setItem('pratice-bdata', JSON.stringify(orgData));
+        localStorage.setItem('pratice-bdata' , JSON.stringify(orgData));
     }
 
     const [bdMode, setBdMode] = useState('L');
-
+    const [pgNum, setPgNum] = useState(1);
+    const [pgPgNum, setPgPgNum] =useState(1);
     const pgBlock = 7;
     const pgPgBlock = 4;
     const totNum = orgData.length;
-
-    const [pgNum, setPgNum] = useState(1);
-    const pgPgNum = useRef(1);
 
     const bindList = () => {
         const tempData = [];
@@ -37,20 +35,20 @@ export function Board(){
             tempData.push(orgData[i]);
         }
 
-        if(orgData.length === 0){
-            return(
+        if (orgData.length === 0) {
+            return (
                 <tr>
-                    <td>데이터가 없습니다.</td>
+                    <td colSpan="5">There is no data.</td>
                 </tr>
-            )
+            );
         }
 
         return tempData.map((v, i) => (
             <div className="board-cont-wrap">
                 <ul>
-                    <li>{i + 1 + initNum}</li>
+                    <li>{ i + 1 + initNum}</li>
                     <li>
-                        <a href="#" data-idx={v.idx}>
+                        <a href="#" data-idx={v.idx} onClick={chgMode}>
                             {v.tit}
                         </a>
                     </li>
@@ -59,118 +57,153 @@ export function Board(){
                     <li>{v.cnt}</li>
                 </ul>
             </div>
-        )); 
+        ));
     };
 
     const pagingLink = () => {
+        let pgCode = [];
+        
         const blockCnt = Math.floor(totNum / pgBlock);
         const blockPad = totNum % pgBlock;
-        const limit = blockCnt + (blockPad===0? 0 : 1);
+        const limit = blockCnt + (blockPad === 0? 0 : 1);
 
         const pgBlockCnt = Math.floor(limit / pgPgBlock);
         const pgBlockPad = limit % pgPgBlock;
-        const pgLimit = pgBlockCnt + (pgBlockPad===0? 0 : 1);
+        const pgLimit = pgBlockCnt + (pgBlockPad === 0? 0 : 1);
 
-        let pgCode = [];
+        let initNum = (pgPgNum - 1) * pgPgBlock;
+        let limitNum = pgPgNum * pgPgBlock;
 
-        let initNum = (pgPgNum.current - 1) * pgPgBlock;
-        let limitNum = pgPgNum.current * pgPgBlock;
-        
         for(let i=initNum; i<limitNum; i++){
-            if(i>=limit) break;
-
+            if(i >= limit) break;
             pgCode[i] = (
-                <Fragment>
+                <Fragment key={i}>
                     {
                         pgNum - 1 === i? (
-                            <b>{i+1}</b>
+                            <b style={{margin : '0 5px'}}>{i+1}</b>
                         ) : (
-                            <a href="#" onClick={chgList}
-                            style={{
-                                margin : '0 10px'
-                            }}>
-                                {i+1}
-                            </a>
+                            <a href="#" style={{margin : '0 5px'}}
+                            onClick={chgList}>{i+1}</a>
                         )
                     }
                 </Fragment>
-            );
+            )
         }
         {
             pgCode.unshift(
-                pgPgNum.current === 1 ? "" : (
+                pgPgNum === 1? (
+                    ""
+                ) : (
                     <Fragment key={-1}>
                         <a href="#"
-                            style={{marginLeft : '5px'}}
+                            style={{marginRight : '5px'}}
+                            title="맨앞으로"
                             onClick={(e) => {
                                 e.preventDefault();
                                 goPaging(1, false);
                             }}>
-                            ≪
-                        </a>
+                                ≪
+                            </a>
                         <a href="#"
-                        style={{marginLeft : '5px'}}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            goPaging(-1, true);
-                        }}>
-                            ◀
-                        </a>
+                            style={{marginRight : '5px'}}
+                            title="앞으로"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                goPaging(-1, true);
+                            }}>
+                                ◀
+                            </a>
                     </Fragment>
                 )
             )
         }
+        
         {
             pgCode.push(
-                pgPgNum.current === pgLimit? "" : (
+                pgPgNum === pgLimit? (
+                    ""
+                ) : (
                     <Fragment key={-2}>
                         <a href="#"
-                        style={{marginRight : '5px'}}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            goPaging(1, true)
-                        }}>
-                            ▶
-                        </a>
+                            style={{marginLeft : '5px'}}
+                            title="뒤로"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                goPaging(1, true);
+                            }}>▶
+                                
+                            </a>
                         <a href="#"
-                        style={{marginRight : '5px'}}
-                        onClick={(e) => {
-                            e.preventDefault();
-                            goPaging(pgLimit, false);
-                        }}>
-                            ≫
-                        </a>
+                            style={{marginLeft : '5px'}}
+                            title="맨뒤로"
+                            onClick={(e) => {
+                                e.preventDefault();
+                                goPaging(pgLimit, false);
+                            }}>
+                                ≫
+                            </a>
                     </Fragment>
                 )
             )
         }
+        
         return pgCode;
     };
 
     const goPaging = (dir, opt) => {
         let newPgPgNum;
-        if(opt)newPgPgNum = dir + pgPgNum.current;
+        if(opt) newPgPgNum = dir + pgPgNum;
         else newPgPgNum = dir;
 
         let newPgNum = (newPgPgNum - 1) * pgPgBlock + 1;
 
-        pgPgNum.current = newPgPgNum;
+        setPgPgNum(newPgPgNum);
         setPgNum(newPgNum);
+
     };
 
     const chgList = (e) => {
         e.preventDefault();
         let currNum = e.target.innerText;
         setPgNum(currNum);
+    }
+
+    // 글읽기
+    const cData = useRef(null);
+    const chgMode = (e) => {
+        e.preventDefault();
+
+        let btxt = e.target.innerText;
+        let modeTxt;
+
+        switch(btxt) {
+            case "목록" :
+                modeTxt = 'L';
+                break;
+            default : 
+                modeTxt = 'R';
+        }
+
+        if(modeTxt === 'R'){
+            let cidx = e.target.getAttribute('data-idx');
+
+            cData.current = orgData.find((v) => {
+                if(Number(v.idx) === Number(cidx)) return true;
+            });
+
+            setBdMode('R');
+        }else if(modeTxt === 'L'){
+            setBdMode('L');
+        }
     };
 
-    
+
     return(
         <>
             {
                 bdMode === 'L' && (
                     <div className="board-wrap">
-                        <h1 className="tit">Question</h1>
+                        {/* 리스트 테이블 박스 */}
                         <div className="dtbl" id="board">
                             <ul>
                                 <li>번호</li>
@@ -185,7 +218,54 @@ export function Board(){
                     </div>
                 )
             }
-            
+            {
+                bdMode === 'R' && (
+                    <div className="dtblview readone">
+                        <h1>게시판</h1>
+                        <div>
+                            <ul>
+                                <li>닉네임</li>
+                                <li>
+                                    <input type="text" className="name" size='20' readOnly value={cData.current.unm} />
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>제목</li>
+                                <li>
+                                    <input type="text" className="subject" size='60' readOnly value={cData.current.tit} />
+                                </li>
+                            </ul>
+                            <ul>
+                                <li>내용</li>
+                                <li>
+                                    {cData.current.cont}
+                                </li>
+                            </ul>
+                        </div>
+                    </div>
+                )
+            }
+            {/* 버튼 그룹박스 */}
+            <div className="dbtl btngrp">
+                <div>
+                    <ul>
+                        {
+                            bdMode === 'L' && (
+                                <button onClick={chgMode}>
+                                    <a href="#">글쓰기</a>
+                                </button>
+                            )
+                        }
+                        {
+                            bdMode === 'R' && (
+                                <button onClick={chgMode}>
+                                    <a href="#">목록</a>
+                                </button>
+                            )
+                        }
+                    </ul>
+                </div>
+            </div>
         </>
     )
 }
