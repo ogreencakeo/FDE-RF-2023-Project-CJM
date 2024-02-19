74f2ab64c65d4f0970641d26b36d8e5f4ee7384d
jimin0427 <cjm0427@hs.ac.kr>
2024-02-19 15:02:16 +0900

    Update index.js

--- pratice_01/etude/js/index.js
+++ pratice_01/etude/js/index.js
@@ -88,5 +88,85 @@ main_img.setAttribute('src', main2Img[randomImg]);
 
 main_img_bx.appendChild(main_img);
 
+// main2 글자 등장
+const main2Tit = document.querySelector('.main-title-bx>img');
+
+const showTit = () => {
+    const main2TitPosition = main2Tit.getBoundingClientRect().top;
+    if (main2TitPosition < window.innerHeight) {
+        main2Tit.classList.add('on')
+    } else {
+        main2Tit.classList.remove('on')
+    }
+};
 
 
+// main3
+const main3Bx = document.querySelector(".main3-wrap");
+temp = [];
+
+main3Data.map((v, i) => {
+    temp[i] = `
+        <div class='main3-bx'>
+            <div class='main3-img'>
+                <img src=${v.img} alt=${v.cont}>
+                <section>
+                    <h2>${v.txt}</h2>
+                    <h3>${v.cont}</h3>
+                </section>
+            </div>
+        </div>
+    `;
+});
+
+main3Bx.innerHTML += temp.join("");
+
+const main3Img = document.querySelectorAll('.main3-bx');
+
+const showMain3Img = () => {
+    main3Img.forEach((v, i) => {
+        const main3ImgPosition = v.getBoundingClientRect().top;
+        if (main3ImgPosition < window.innerHeight) {
+            // 0.15초 간격으로 나타나도록 setTimeout 사용
+            setTimeout(() => {
+                v.classList.add('on');
+            }, i * 150);
+        } else {
+            v.classList.remove('on');
+        }
+    });
+};
+
+window.addEventListener('scroll', showMain3Img);
+
+// main4
+const main4Bx = document.querySelector('.main4-img-l');
+
+temp = [];
+
+main4Data.map((v, i) => {
+    temp[i] = `
+        <div class='main4Bx'>
+            <div class='main4Bx-img' onmouseover='changeImgFn(${i})' onmouseout='restoreImg(${i})'>
+                <img src=${v.img} id='image${i}'/>
+            </div>
+            <section>
+                <h2>${v.cont}</h2>
+                <h2>${v.txt}</h2>
+                <h2>${v.price}</h2>
+            </section>
+        </div>
+    `
+});
+
+main4Bx.innerHTML += temp.join('');
+
+const changeImgFn = (i) => {
+    const img = document.getElementById(`image${i}`);
+    img.src = `../image/main/main4/${i + 1}-2.jpg`;
+};
+
+const restoreImg = (i) => {
+    const img = document.getElementById(`image${i}`);
+    img.src = main4Data[i].img;
+}
